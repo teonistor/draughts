e98570f1978f0eda2f3080bd40c379810fa25430
Teo G Nistor <teo.g.nistor@gmail.com>
2020-05-15 21:40:06 +0100

    devschess: Building blocks: Piece, Player

---
+++ src/main/java/io/github/teonistor/devschess/piece/Piece.java
@@ -0,0 +1,11 @@
+package io.github.teonistor.devschess.piece;
+
+import io.github.teonistor.devschess.Player;
+import io.github.teonistor.devschess.move.Move;
+
+import java.util.stream.Stream;
+
+public interface Piece {
+    Player getPlayer();
+    Stream<Move> computePossibleMoves();
+}
