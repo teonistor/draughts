93fedf4cd42b84c67f71f2063cae988eccbbc5fa
Teo G Nistor <teo.g.nistor@gmail.com>
2020-06-09 22:33:11 +0100

    chess: Game, the centrepiece!

---
+++ src/main/java/io/github/teonistor/devschess/Game.java
@@ -0,0 +1,115 @@
+package io.github.teonistor.devschess;
+
+import com.google.common.annotations.VisibleForTesting;
+import io.github.teonistor.devschess.board.Board;
+import io.github.teonistor.devschess.board.Position;
+import io.github.teonistor.devschess.inter.Input;
+import io.github.teonistor.devschess.inter.View;
+import io.github.teonistor.devschess.move.Move;
+import io.github.teonistor.devschess.piece.Piece;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.HashSet;
+import io.vavr.collection.List;
+import io.vavr.collection.Map;
+import io.vavr.collection.Set;
+import io.vavr.control.Option;
+
+import java.util.stream.Stream;
+
+import static io.github.teonistor.devschess.Player.White;
+
+public class Game {
+    private static final Piece NO_PIECE = new Piece(){
+        public Player getPlayer() {
+            return null;
+        }
+        public Stream<Move> computePossibleMoves(Position from) {
+            return Stream.empty();
+        }
+    };
+
+    private final Input[] inputs;
+    private final View views;
+
+    public Game(Input white, Input black, View...views) {
+        inputs = new Input[2];
+        inputs[Player.White.ordinal()] = white;
+        inputs[Player.Black.ordinal()] = black;
+
+        this.views = new View() {
+            private Iterable<View> vi = List.of(views);
+
+            public void refresh(Map<Position, Piece> board, Player player, Set<Piece> capturedPieces, Position source, Set<Position> targets) {
+                vi.forEach(v -> v.refresh(board, player, capturedPieces, source, targets));
+            }
+
+            public void announce(String message) {
+                vi.forEach(v -> v.announce(message));
+            }
+        };
+    }
+
+    public void play() {
+        GameState state = initialState();
+        views.refresh(state.getBoard(), state.getPlayer(), state.getCapturedPieces(), Position.OutOfBoard, HashSet.empty());
+
+        while (!isOver(state)) {
+            state = takeFirstInput(state);
+        }
+    }
+
+    private GameState takeFirstInput(GameState state) {
+        final Position source = inputs[state.getPlayer().ordinal()].takeInput();
+        final Option<Piece> sourcePiece = state.getBoard().get(source).filter(p -> state.getPlayer().equals(p.getPlayer()));
+
+        if (sourcePiece.isDefined()) {
+            final Map<Position,Move> moves = sourcePiece.get().computePossibleMoves(source)
+                    .filter(move -> move.validate(state.getBoard().toJavaMap()))
+                    .collect(HashMap.collector(Move::getTo));
+            views.refresh(state.getBoard(), state.getPlayer(), state.getCapturedPieces(), source, moves.keySet());
+
+            return takeSecondInput(state, source, sourcePiece.get(), moves);
+        }
+
+        if(source != Position.OutOfBoard) {
+            views.announce("Invalid pickup: " + source);
+        }
+        return takeFirstInput(state);
+    }
+
+    private GameState takeSecondInput(GameState state, Position source, Piece sourcePiece, Map<Position, Move> moves) {
+        final Position target = inputs[state.getPlayer().ordinal()].takeInput();
+
+        if (moves.containsKey(target)) {
+            views.announce(String.format("%s moves: %s - %s", state.getPlayer(), source, target));
+
+            // TODO captures
+            return state.advance(bridgeTheGap(moves.get(target).get(), state.getBoard()));
+        }
+
+        if(source == Position.OutOfBoard) {
+            return takeFirstInput(state);
+        }
+        views.announce(String.format("Invalid move: %s - %s", source, target));
+        return takeSecondInput(state, source, sourcePiece, moves);
+    }
+
+    private HashMap<Position, Piece> bridgeTheGap(Move move, Map<Position, Piece> board) {
+        final java.util.Map<Position, Piece> tmp = board.toJavaMap();
+        move.execute(tmp);
+        return HashMap.ofAll(tmp);
+    }
+
+    @VisibleForTesting
+    GameState initialState() {
+        return new GameState(Board.initialSetup(), White, HashSet.empty());
+    }
+
+
+    @VisibleForTesting
+    boolean isOver(final GameState state) {
+        // TODO The entire state is probably not needed
+        // TODO We need a GameOverChecker :P
+        return false;
+    }
+}
---
+++ src/test/java/io/github/teonistor/devschess/GameTest.java
@@ -0,0 +1,102 @@
+package io.github.teonistor.devschess;
+
+import io.github.teonistor.devschess.board.Position;
+import io.github.teonistor.devschess.inter.Input;
+import io.github.teonistor.devschess.inter.View;
+import io.github.teonistor.devschess.move.Move;
+import io.github.teonistor.devschess.piece.Piece;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.HashSet;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
+
+import static io.github.teonistor.devschess.Player.White;
+import static io.github.teonistor.devschess.board.Position.A1;
+import static io.github.teonistor.devschess.board.Position.A3;
+import static io.github.teonistor.devschess.board.Position.B3;
+import static io.github.teonistor.devschess.board.Position.G7;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+class GameTest {
+
+    private final Input white = mock(Input.class);
+    private final Input black = mock(Input.class);
+    private final View view = mock(View.class);
+    private final Piece piece = mock(Piece.class);
+    private final HashMap<Position, Piece> board = HashMap.of(A1, piece);
+    private final GameState state = spy(new GameState(board, White, HashSet.empty()));
+    private final Move move = mock(Move.class);
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = spy(new Game(white, black, view));
+        when(game.initialState()).thenReturn(state);
+        when(state.advance(any())).thenReturn(state);
+    }
+
+    @Test
+    void playOneRound() {
+        when(game.isOver(state)).thenReturn(false).thenReturn(true);
+        when(white.takeInput()).thenReturn(A1).thenReturn(B3);
+        when(piece.getPlayer()).thenReturn(White);
+        when(piece.computePossibleMoves(A1)).thenReturn(Stream.of(move));
+        when(move.validate(any())).thenReturn(true);
+        when(move.getTo()).thenReturn(B3);
+//        when(move.) TODO when the gap is bridged, stub the move execution
+        game.play();
+
+        verify(game, times(2)).isOver(state);
+        verify(white, times(2)).takeInput();
+        verify(view, times(2)).refresh(eq(board), any(), eq(HashSet.empty()), any(), any());
+        verify(view).announce("White moves: A1 - B3");
+        verify(piece).getPlayer();
+        verify(piece).computePossibleMoves(A1);
+        verify(move).validate(any()); // TODO when the gap is bridged, pass map
+        verify(move).getTo();
+        verify(move).execute(any()); // TODO when the gap is bridged, pass map
+        verify(state).advance(board);
+    }
+
+    @Test
+    void playOneRoundWithBogusInputs() {
+        when(game.isOver(state)).thenReturn(false).thenReturn(true);
+        when(white.takeInput()).thenReturn(A3).thenReturn(A1).thenReturn(G7).thenReturn(B3);
+        when(piece.getPlayer()).thenReturn(White);
+        when(piece.computePossibleMoves(A1)).thenReturn(Stream.of(move));
+        when(move.validate(any())).thenReturn(true);
+        when(move.getTo()).thenReturn(B3);
+//        when(move.) TODO when the gap is bridged, stub the move execution
+        game.play();
+
+        verify(game, times(2)).isOver(state);
+        verify(white, times(4)).takeInput();
+        verify(view, times(2)).refresh(eq(board), any(), eq(HashSet.empty()), any(), any());
+        verify(view).announce("Invalid pickup: A3");
+        verify(view).announce("Invalid move: A1 - G7");
+        verify(view).announce("White moves: A1 - B3");
+        verify(piece).getPlayer();
+        verify(piece).computePossibleMoves(A1);
+        verify(move).validate(any()); // TODO when the gap is bridged, pass map
+        verify(move).getTo();
+        verify(move).execute(any()); // TODO when the gap is bridged, pass map
+        verify(state).advance(board);
+    }
+
+    // TODO Game deserves, like, a couple dozen tests
+
+    @AfterEach
+    void tearDown() {
+        verifyNoMoreInteractions(white, black, view, piece, move);
+    }
+}
