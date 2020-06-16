6dbc978a1299e8fbd122401a0c469ad3e3f649b4
Teo G Nistor <teo.g.nistor@gmail.com>
2020-06-16 20:35:53 +0100

    chess: Castle

---
+++ src/main/java/io/github/teonistor/chess/move/Castle.java
@@ -0,0 +1,59 @@
+package io.github.teonistor.chess.move;
+
+import io.github.teonistor.chess.board.Position;
+import io.github.teonistor.chess.core.Player;
+import io.github.teonistor.chess.core.UnderAttackRule;
+import io.github.teonistor.chess.piece.King;
+import io.github.teonistor.chess.piece.Piece;
+import io.github.teonistor.chess.piece.Rook;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.HashSet;
+import io.vavr.collection.Map;
+import io.vavr.control.Option;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static io.github.teonistor.chess.board.Position.*;
+
+
+@AllArgsConstructor
+public class Castle implements Move {
+    public static final HashMap<Position,HashSet<Position>> mustNotBeUnderAttackByTarget = HashMap.of(
+            G1, HashSet.of(E1, F1, G1), C1, HashSet.of(C1, D1, E1),
+            G8, HashSet.of(E8, F8, G8), C8, HashSet.of(C8, D8, E8));
+    public static final HashMap<Position,HashSet<Position>> mustBeEmptyByTarget = HashMap.of(
+            G1, HashSet.of(F1, G1), C1, HashSet.of(B1, C1, D1),
+            G8, HashSet.of(F8, G8), C8, HashSet.of(B8, C8, D8));
+    public static final HashMap<Position,Position> rookPositionsByTarget = HashMap.of(
+            G1, H1, C1, A1,
+            G8, H8, C8, A8);
+    public static final HashMap<Position,Position> rookTargetsByTarget = HashMap.of(
+            G1, F1, C1, D1,
+            G8, F8, C8, D8);
+
+    private final @NonNull @Getter Position from;
+    private final @NonNull @Getter Position to;
+    private final @NonNull Player player;
+    private final @NonNull UnderAttackRule underAttackRule;
+    
+    @Override
+    public boolean validate(Map<Position, Piece> board) {
+        return mustBeEmptyByTarget.get(to).get().toStream().map(board::get).filter(Option::isDefined).isEmpty()
+            && rookPositionsByTarget.get(to).flatMap(board::get).filter(piece -> piece.getPlayer() == player && piece.getClass().equals(Rook.class)).isDefined()
+            && mustNotBeUnderAttackByTarget.get(to).get().toStream().filter(position -> underAttackRule.checkAttack(board, position, player)).isEmpty();
+    }
+
+    @Override
+    public <T> T execute(Map<Position, Piece> board, Function<Map<Position, Piece>, T> nonCapturingCallback, BiFunction<Map<Position, Piece>, Piece, T> capturingCallback) {
+        final Position rookFrom = rookPositionsByTarget.get(to).get();
+        final Position rookTo = rookTargetsByTarget.get(to).get();
+
+        return nonCapturingCallback.apply(board.remove(from).remove(rookFrom)
+              .put(to, new King(player))
+              .put(rookTo, board.get(rookFrom).get()));
+    }
+}
---
+++ src/test/java/io/github/teonistor/chess/move/CastleTest.java
@@ -0,0 +1,109 @@
+package io.github.teonistor.chess.move;
+
+import io.github.teonistor.chess.board.Position;
+import io.github.teonistor.chess.core.Player;
+import io.github.teonistor.chess.core.UnderAttackRule;
+import io.github.teonistor.chess.piece.King;
+import io.github.teonistor.chess.piece.Knight;
+import io.github.teonistor.chess.piece.Piece;
+import io.github.teonistor.chess.piece.Rook;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.Map;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+
+class CastleTest extends MoveTest{
+
+    private final UnderAttackRule rule = mock(UnderAttackRule.class);
+
+    @ParameterizedTest
+    @CsvSource({"E1,G1,White,H1,E1",
+                "E1,G1,White,H1,F1",
+                "E1,G1,White,H1,G1",
+                "E1,C1,White,A1,E1",
+                "E1,C1,White,A1,D1",
+                "E1,C1,White,A1,C1",
+                "E8,G8,Black,H8,E8",
+                "E8,G8,Black,H8,F8",
+                "E8,G8,Black,H8,G8",
+                "E8,C8,Black,A8,E8",
+                "E8,C8,Black,A8,D8",
+                "E8,C8,Black,A8,C8"})
+    void validateInvalidBecauseUnderAttack(Position from, Position to, Player player, Position rookPos, Position underAttack) {
+        HashMap<Position, Piece> board = HashMap.of(rookPos, new Rook(player));
+        when(rule.checkAttack(board, underAttack, player)).thenReturn(true);
+
+        assertThat(new Castle(from, to, player, rule).validate(board)).isFalse();
+        verify(rule, atMost(3)).checkAttack(eq(board), any(), eq(player));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"E1,G1,White", "E1,C1,White",
+                "E8,G8,Black", "E8,C8,Black"})
+    void validateInvalidBecauseNoRook(Position from, Position to, Player player) {
+        HashMap<Position, Piece> board = HashMap.empty();
+
+        assertThat(new Castle(from, to, player, rule).validate(board)).isFalse();
+        verify(rule, atMost(3)).checkAttack(eq(board), any(), eq(player));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"E1,G1,White,H1,F1",
+                "E1,G1,White,H1,G1",
+                "E1,C1,White,A1,D1",
+                "E1,C1,White,A1,C1",
+                "E1,C1,White,A1,B1",
+                "E8,G8,Black,H8,F8",
+                "E8,G8,Black,H8,G8",
+                "E8,C8,Black,A8,D8",
+                "E8,C8,Black,A8,C8",
+                "E8,C8,Black,A8,B8"})
+    void validateInvalidBecauseOccupied(Position from, Position to, Player player, Position rookPos, Position occupied) {
+        HashMap<Position, Piece> board = HashMap.of(rookPos, new Rook(player), occupied, new Knight(player));
+
+        assertThat(new Castle(from, to, player, rule).validate(board)).isFalse();
+        verify(rule, atMost(3)).checkAttack(eq(board), any(), eq(player));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"E1,G1,White,H1", "E1,C1,White,A1",
+                "E8,G8,Black,H8", "E8,C8,Black,A8"})
+    void validateValid(Position from, Position to, Player player, Position rookPos) {
+        HashMap<Position, Piece> board = HashMap.of(rookPos, new Rook(player));
+
+        assertThat(new Castle(from, to, player, rule).validate(board)).isTrue();
+        verify(rule, times(3)).checkAttack(eq(board), any(), eq(player));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"E1,G1,White,H1,F1", "E1,C1,White,A1,D1",
+                "E8,G8,Black,H8,F8", "E8,C8,Black,A8,D8"})
+    void execute(Position from, Position to, Player player, Position rookFrom, Position rookTo) {
+        HashMap<Position, Piece> board = HashMap.of(from, new King(player), rookFrom, new Rook(player));
+
+        Map<Position, Piece> output = new Castle(from, to, player, rule).execute(board, nonCapturingReturnBoard, captureNotExpected);
+
+        assertThat(output.get(to).get()).isExactlyInstanceOf(King.class);
+        assertThat(output.get(to).get().getPlayer()).isEqualTo(player);
+        assertThat(output.get(rookTo).get()).isExactlyInstanceOf(Rook.class);
+        assertThat(output.get(rookTo).get().getPlayer()).isEqualTo(player);
+    }
+
+    @AfterEach
+    void tearDown() {
+        verifyNoMoreInteractions(rule);
+    }
+}
