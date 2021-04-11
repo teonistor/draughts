0c208351bafbbd1bb66ccd0173b2597c15b573ef
Teo G Nistor <teo.g.nistor@gmail.com>
2021-04-11 16:56:42 +0100

    Extract out front page controller logic

---
+++ src/main/java/io/github/teonistor/chess/spring/ChessFrontpageCtrl.java
@@ -0,0 +1,34 @@
+package io.github.teonistor.chess.spring;
+
+import io.github.teonistor.chess.core.Player;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("chess-api")
+public class ChessFrontpageCtrl {
+
+    @RequestMapping("assign-hotseat")
+    RedirectView assignHotseat(final HttpServletResponse response) {
+        return assignPlayer(response, "Hotseat");
+    }
+
+    @RequestMapping("assign-black")
+    RedirectView assignBlack(final HttpServletResponse response) {
+        return assignPlayer(response, Player.Black);
+    }
+
+    @RequestMapping("assign-white")
+    RedirectView assignWhite(final HttpServletResponse response) {
+        return assignPlayer(response, Player.White);
+    }
+
+    private RedirectView assignPlayer(final HttpServletResponse response, final Object player) {
+        response.addCookie(new Cookie("player", player.toString()));
+        return new RedirectView("/chess/game.html");
+    }
+}
