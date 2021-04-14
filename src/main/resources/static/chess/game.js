39c3902b2dfb24b836ee15f14c3880d175c000d9
Teo G Nistor <teo.g.nistor@gmail.com>
2021-04-14 22:27:06 +0100

    More interface improvements

--- src/main/resources/static/chess/game.js
+++ src/main/resources/static/chess/game.js
@@ -6,31 +6,53 @@ new Vue({
         stompClient: null,
         restCallsGoing: 0,
 
-        pieceBox: {
-            // https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces
-            BP: 'https://upload.wikimedia.org/wikipedia/commons/4/49/CommonerB_Transparent.svg',
-            BR: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg',
-            BN: 'https://upload.wikimedia.org/wikipedia/commons/c/c8/Chess_Udt45.svg',
-            BB: 'https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg',
-            BQ: 'https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg',
-            BK: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg',
-            WP: 'https://upload.wikimedia.org/wikipedia/commons/7/7d/Commoner_Transparent.svg',
-            WR: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg',
-            WN: 'https://upload.wikimedia.org/wikipedia/commons/8/89/Chess_Ult45.svg',
-            WB: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg',
-            WQ: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg',
-            WK: 'https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg'
-        },
-        positions: [
-            ['A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8'],
-            ['A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7'],
-            ['A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6'],
-            ['A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5'],
-            ['A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4'],
-            ['A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3'],
-            ['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2'],
-            ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1']
-        ],
+        pieceBox: { // https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces
+            true: {
+                BP: 'https://upload.wikimedia.org/wikipedia/commons/4/49/CommonerB_Transparent.svg',
+                BR: 'https://upload.wikimedia.org/wikipedia/commons/f/fa/Chess_Mdt45.svg',
+                BN: 'https://upload.wikimedia.org/wikipedia/commons/c/c8/Chess_Udt45.svg',
+                BB: 'https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg',
+                BQ: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Chess_qdt26.svg',
+                BK: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg',
+                WP: 'https://upload.wikimedia.org/wikipedia/commons/7/7d/Commoner_Transparent.svg',
+                WR: 'https://upload.wikimedia.org/wikipedia/commons/2/20/Chess_Mlt45.svg',
+                WN: 'https://upload.wikimedia.org/wikipedia/commons/8/89/Chess_Ult45.svg',
+                WB: 'https://upload.wikimedia.org/wikipedia/commons/5/50/Chess_tile_bl.svg',
+                WQ: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg',
+                WK: 'https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg'},
+            false: {
+                BP: 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg',
+                BR: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg',
+                BN: 'https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg',
+                BB: 'https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg',
+                BQ: 'https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg',
+                BK: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg',
+                WP: 'https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg',
+                WR: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg',
+                WN: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg',
+                WB: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg',
+                WQ: 'https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg',
+                WK: 'https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg'}},
+
+        positions: {
+            true: [
+                ['H1', 'G1', 'F1', 'E1', 'D1', 'C1', 'B1', 'A1'],
+                ['H2', 'G2', 'F2', 'E2', 'D2', 'C2', 'B2', 'A2'],
+                ['H3', 'G3', 'F3', 'E3', 'D3', 'C3', 'B3', 'A3'],
+                ['H4', 'G4', 'F4', 'E4', 'D4', 'C4', 'B4', 'A4'],
+                ['H5', 'G5', 'F5', 'E5', 'D5', 'C5', 'B5', 'A5'],
+                ['H6', 'G6', 'F6', 'E6', 'D6', 'C6', 'B6', 'A6'],
+                ['H7', 'G7', 'F7', 'E7', 'D7', 'C7', 'B7', 'A7'],
+                ['H8', 'G8', 'F8', 'E8', 'D8', 'C8', 'B8', 'A8']],
+            false: [
+                ['A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8'],
+                ['A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7'],
+                ['A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6'],
+                ['A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5'],
+                ['A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4'],
+                ['A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3'],
+                ['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2'],
+                ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1']]},
 
         board: {},
         capturedPieces: [],
