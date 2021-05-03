3fb026af09165b72994edb950e671b8c5d1d2cc8
Teo G Nistor <teo.g.nistor@gmail.com>
2021-05-03 19:05:05 +0100

    Pawn emits Promotion

--- src/main/java/io/github/teonistor/chess/piece/Pawn.java
+++ src/main/java/io/github/teonistor/chess/piece/Pawn.java
@@ -6,67 +6,72 @@ import io.github.teonistor.chess.move.CapturingMove;
 import io.github.teonistor.chess.move.EmptyPositionMove;
 import io.github.teonistor.chess.move.EnPassant;
 import io.github.teonistor.chess.move.Move;
+import io.github.teonistor.chess.move.Promotion;
+import io.github.teonistor.chess.move.SingleOutcomeMove;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.Map;
 import io.vavr.collection.Stream;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import static io.github.teonistor.chess.board.Position.OutOfBoard;
 
 
 public class Pawn extends Piece {
+    private static final Map<Player, Function<Position, Stream<? extends Move>>> computeBasedOnPlayer = HashMap.of(
+            Player.White, Pawn::computePossibleMovesWhite,
+            Player.Black, Pawn::computePossibleMovesBlack);
 
-    public Pawn(Player player) {
+    public Pawn(final Player player) {
         super(player);
     }
 
     @Override
     public Stream<Move> computePossibleMoves(final Position from) {
-        return computePossibleMoves0(from).filter(move -> !OutOfBoard.equals(move.getTo()));
+        return Stream.narrow(Stream.of(getPlayer())
+             . map(computeBasedOnPlayer)
+             . flatMap(computation -> computation.apply(from))
+             . filter(move -> !OutOfBoard.equals(move.getTo())));
     }
 
-    private Stream<Move> computePossibleMoves0(final Position from) {
-        switch (getPlayer()) {
-            case White:
+    private static Stream<? extends Move> computePossibleMovesWhite(final Position from) {
+        final Stream<SingleOutcomeMove> common = Stream.of(
+                new EmptyPositionMove(from, from.up()),
+                new CapturingMove(from, from.up().right()),
+                new CapturingMove(from, from.up().left()));
 
-                if (from.toString().charAt(1) == '2') {
-                    return Stream.of(
-                            new EmptyPositionMove(from, from.up()),
-                            new EmptyPositionMove(from, from.up().up(), from.up()),
-                            new CapturingMove    (from, from.up().right()),
-                            new CapturingMove    (from, from.up().left()));
-                } else if (from.toString().charAt(1) == '5') {
-                    return Stream.of(
-                            new EmptyPositionMove(from, from.up()),
-                            new CapturingMove    (from, from.up().right()),
-                            new CapturingMove    (from, from.up().left()),
-                            new EnPassant        (from, from.up().right(), from.right(), from.up().up().right()),
-                            new EnPassant        (from, from.up().left(), from.left(), from.up().up().left()));
-                } else {
-                    return Stream.of(
-                            new EmptyPositionMove(from, from.up()),
-                            new CapturingMove    (from, from.up().right()),
-                            new CapturingMove    (from, from.up().left()));
-                }
+        final SingleOutcomeMove startingExtra = new EmptyPositionMove(from, from.up().up(), from.up());
 
-            case Black:
-                if (from.toString().charAt(1) == '7') {
-                    return Stream.of(
-                            new EmptyPositionMove(from, from.down()),
-                            new EmptyPositionMove(from, from.down().down(), from.down()),
-                            new CapturingMove    (from, from.down().right()),
-                            new CapturingMove    (from, from.down().left()));
-                } else if (from.toString().charAt(1) == '4') {
-                    return Stream.of(
-                            new EmptyPositionMove(from, from.down()),
-                            new CapturingMove    (from, from.down().right()),
-                            new CapturingMove    (from, from.down().left()),
-                            new EnPassant        (from, from.down().right(), from.right(), from.down().down().right()),
-                            new EnPassant        (from, from.down().left(), from.left(), from.down().down().left()));
-                } else {
-                    return Stream.of(
-                            new EmptyPositionMove(from, from.down()),
-                            new CapturingMove    (from, from.down().right()),
-                            new CapturingMove    (from, from.down().left()));
-                }
-        }
-        throw new IllegalStateException("Should never happen");
+        final Stream<SingleOutcomeMove> enPassantExtra = Stream.of(
+                new EnPassant(from, from.up().right(), from.right(), from.up().up().right()),
+                new EnPassant(from, from.up().left(), from.left(), from.up().up().left()));
+
+        final char line = from.toString().charAt(1);
+
+        if (line == '2') return common.append(startingExtra);
+        if (line == '5') return common.appendAll(enPassantExtra);
+        if (line == '7') return common.map(Promotion::new);
+        return common;
     }
+
+    private static Stream<? extends Move> computePossibleMovesBlack(final Position from) {
+        final Stream<SingleOutcomeMove> common = Stream.of(
+                new EmptyPositionMove(from, from.down()),
+                new CapturingMove(from, from.down().right()),
+                new CapturingMove(from, from.down().left()));
+
+        final char line = from.toString().charAt(1);
+
+        BiFunction<Stream<? extends SingleOutcomeMove>, Character, Stream<? extends  Move> > ff;
+
+        if (line == '7') return common.append(new EmptyPositionMove(from, from.down().down(), from.down()));
+        if (line == '4') return common.appendAll(Stream.<SingleOutcomeMove>of(
+                        new EnPassant(from, from.down().right(), from.right(), from.down().down().right()),
+                        new EnPassant(from, from.down().left(), from.left(), from.down().down().left())));
+        if (line == '2') return common.map(Promotion::new);
+
+        return common;
+    }
+
 }
--- src/test/java/io/github/teonistor/chess/piece/PawnTest.java
+++ src/test/java/io/github/teonistor/chess/piece/PawnTest.java
@@ -4,8 +4,48 @@ import io.github.teonistor.chess.core.UnderAttackRule;
 import io.github.teonistor.chess.move.CapturingMove;
 import io.github.teonistor.chess.move.EmptyPositionMove;
 import io.github.teonistor.chess.move.EnPassant;
+import io.github.teonistor.chess.move.Promotion;
 import org.junit.jupiter.api.Test;
-import static io.github.teonistor.chess.board.Position.*;
+
+import static io.github.teonistor.chess.board.Position.A1;
+import static io.github.teonistor.chess.board.Position.A2;
+import static io.github.teonistor.chess.board.Position.A3;
+import static io.github.teonistor.chess.board.Position.A4;
+import static io.github.teonistor.chess.board.Position.A5;
+import static io.github.teonistor.chess.board.Position.A6;
+import static io.github.teonistor.chess.board.Position.A7;
+import static io.github.teonistor.chess.board.Position.B1;
+import static io.github.teonistor.chess.board.Position.B2;
+import static io.github.teonistor.chess.board.Position.B3;
+import static io.github.teonistor.chess.board.Position.B5;
+import static io.github.teonistor.chess.board.Position.B6;
+import static io.github.teonistor.chess.board.Position.C1;
+import static io.github.teonistor.chess.board.Position.C2;
+import static io.github.teonistor.chess.board.Position.C3;
+import static io.github.teonistor.chess.board.Position.C4;
+import static io.github.teonistor.chess.board.Position.C6;
+import static io.github.teonistor.chess.board.Position.C8;
+import static io.github.teonistor.chess.board.Position.D3;
+import static io.github.teonistor.chess.board.Position.D5;
+import static io.github.teonistor.chess.board.Position.D6;
+import static io.github.teonistor.chess.board.Position.D7;
+import static io.github.teonistor.chess.board.Position.D8;
+import static io.github.teonistor.chess.board.Position.E4;
+import static io.github.teonistor.chess.board.Position.E5;
+import static io.github.teonistor.chess.board.Position.E6;
+import static io.github.teonistor.chess.board.Position.E8;
+import static io.github.teonistor.chess.board.Position.F3;
+import static io.github.teonistor.chess.board.Position.F5;
+import static io.github.teonistor.chess.board.Position.G3;
+import static io.github.teonistor.chess.board.Position.G4;
+import static io.github.teonistor.chess.board.Position.G5;
+import static io.github.teonistor.chess.board.Position.G6;
+import static io.github.teonistor.chess.board.Position.H2;
+import static io.github.teonistor.chess.board.Position.H3;
+import static io.github.teonistor.chess.board.Position.H4;
+import static io.github.teonistor.chess.board.Position.H5;
+import static io.github.teonistor.chess.board.Position.H6;
+import static io.github.teonistor.chess.board.Position.H7;
 import static org.mockito.Mockito.mock;
 
 
@@ -21,6 +61,7 @@ class PawnTest extends PieceTest {
         assertMove(C2, EmptyPositionMove.class,C3,C4, CapturingMove.class,B3,D3);
         assertMove(H2, EmptyPositionMove.class,H3,H4, CapturingMove.class,G3);
         assertMove(E4, EmptyPositionMove.class,E5,    CapturingMove.class,D5,F5);
+        assertMove(D7, Promotion.class, C8, D8, E8);
 
         assertj.assertAll();
     }
@@ -34,6 +75,7 @@ class PawnTest extends PieceTest {
         assertMove(H7, EmptyPositionMove.class,H5,H6, CapturingMove.class,G6);
         assertMove(G6, EmptyPositionMove.class,G5,    CapturingMove.class,F5,H5);
         assertMove(G4, EmptyPositionMove.class,G3,    CapturingMove.class,F3,H3, EnPassant.class,F3,H3);
+        assertMove(B2, Promotion.class, A1, B1, C1);
 
         assertj.assertAll();
     }
