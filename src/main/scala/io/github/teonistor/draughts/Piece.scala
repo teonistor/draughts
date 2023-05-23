1fc9673f7cf9753482217edea21baed09fa7a78a
Teodor G Nistor <teodor@nistor.uk>
2023-05-23 22:48:59 +0100

    Piece

---
+++ src/main/scala/io/github/teonistor/draughts/Piece.scala
@@ -0,0 +1,43 @@
+package io.github.teonistor.draughts
+
+import io.github.teonistor.draughts.move.Move
+
+sealed trait Piece {
+  def emitMoves(from: Position): LazyList[Move]
+  def promote: Piece
+}
+
+object Piece {
+  private val `+-1` = LazyList(1, -1)
+
+  // TODO Ideally the concept of "forward" would be relegated to the board (via the specific player) but the static initialisaiton implications of that are asinine
+
+  val blackKing:Piece = new King
+  val blackPeon:Piece = new Peon(_-1, blackKing)
+  val whiteKing:Piece = new King
+  val whitePeon:Piece = new Peon(_+1, whiteKing)
+
+  private class King extends Piece {
+    override def emitMoves(from: Position): LazyList[Move] =
+      `+-1`.flatMap(dx =>
+        `+-1`.flatMap(dy =>
+          emitMovesInOneDirection(from, dx+_, dy+_)))
+
+    override def promote: Piece = this
+  }
+
+  private class Peon(forward: Int=>Int, val promote: Piece) extends Piece {
+    override def emitMoves(from: Position): LazyList[Move] =
+      `+-1`.flatMap(dx =>
+        emitMovesInOneDirection(from, dx+_, forward))
+  }
+
+  private def emitMovesInOneDirection(from: Position, dx: Int=>Int, dy: Int=>Int): List[Move] = {
+    val x1 = dx(from.x)
+    val y1 = dy(from.y)
+    val p1 = Position(x1, y1)
+    List(
+      Move.Sliding(from, p1),
+      Move.Jumping(from, p1, Position(dx(x1), dy(y1))))
+  }
+}
---
+++ src/test/scala/io/github/teonistor/draughts/PieceTest.scala
@@ -0,0 +1,49 @@
+package io.github.teonistor.draughts
+
+import io.github.teonistor.draughts.move.Move
+import org.scalatest.funsuite.AnyFunSuite
+
+class PieceTest extends AnyFunSuite {
+
+  test("kings move where they please") {
+    List(Piece.blackKing, Piece.whiteKing).foreach(piece =>
+      assert(piece.emitMoves(Position(3,4)).toSet == Set(
+        Move.Sliding(Position(3,4), Position(4,5)),
+        Move.Sliding(Position(3,4), Position(4,3)),
+        Move.Sliding(Position(3,4), Position(2,5)),
+        Move.Sliding(Position(3,4), Position(2,3)),
+        Move.Jumping(Position(3,4), Position(4,5), Position(5,6)),
+        Move.Jumping(Position(3,4), Position(4,3), Position(5,2)),
+        Move.Jumping(Position(3,4), Position(2,5), Position(1,6)),
+        Move.Jumping(Position(3,4), Position(2,3), Position(1,2)))))
+  }
+
+  test("black peon moves down") {
+    assert(Piece.blackPeon.emitMoves(Position(1,7)).toSet == Set(
+      Move.Sliding(Position(1,7), Position(0,6)),
+      Move.Sliding(Position(1,7), Position(2,6)),
+      Move.Jumping(Position(1,7), Position(0,6), Position(-1,5)),
+      Move.Jumping(Position(1,7), Position(2,6), Position(3,5))))
+  }
+
+  test("white peon moves up") {
+    assert(Piece.whitePeon.emitMoves(Position(1,7)).toSet == Set(
+      Move.Sliding(Position(1,7), Position(0,8)),
+      Move.Sliding(Position(1,7), Position(2,8)),
+      Move.Jumping(Position(1,7), Position(0,8), Position(-1,9)),
+      Move.Jumping(Position(1,7), Position(2,8), Position(3,9))))
+  }
+
+  test("kings promote to themselves") {
+    List(Piece.blackKing, Piece.whiteKing).foreach(piece =>
+      assert(piece.promote == piece))
+  }
+
+  test("black peon promotea to black king") {
+    assert(Piece.blackPeon.promote == Piece.blackKing)
+  }
+
+  test("white peon promotea to white king") {
+    assert(Piece.whitePeon.promote == Piece.whiteKing)
+  }
+}
