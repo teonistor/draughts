a86f6c6bb4a79b137a20e0a7cb473f1f13375b12
Teodor G Nistor <teodor@nistor.uk>
2023-05-24 20:45:54 +0100

    Make Piece emit moves keyed on the "to" position

--- src/main/scala/io/github/teonistor/draughts/Piece.scala
+++ src/main/scala/io/github/teonistor/draughts/Piece.scala
@@ -3,7 +3,7 @@ package io.github.teonistor.draughts
 import io.github.teonistor.draughts.move.Move
 
 sealed trait Piece {
-  def emitMoves(from: Position): LazyList[Move]
+  def emitMoves(from: Position): Map[Position, Move]
   def promote: Piece
 }
 
@@ -18,26 +18,27 @@ object Piece {
   val whitePeon:Piece = new Peon(_+1, whiteKing)
 
   private class King extends Piece {
-    override def emitMoves(from: Position): LazyList[Move] =
+    override def emitMoves(from: Position): Map[Position, Move] =
       `+-1`.flatMap(dx =>
         `+-1`.flatMap(dy =>
-          emitMovesInOneDirection(from, dx+_, dy+_)))
+          emitMovesInOneDirection(from, dx+_, dy+_))).toMap
 
     override def promote: Piece = this
   }
 
   private class Peon(forward: Int=>Int, val promote: Piece) extends Piece {
-    override def emitMoves(from: Position): LazyList[Move] =
+    override def emitMoves(from: Position): Map[Position, Move] =
       `+-1`.flatMap(dx =>
-        emitMovesInOneDirection(from, dx+_, forward))
+        emitMovesInOneDirection(from, dx+_, forward)).toMap
   }
 
-  private def emitMovesInOneDirection(from: Position, dx: Int=>Int, dy: Int=>Int): List[Move] = {
+  private def emitMovesInOneDirection(from: Position, dx: Int=>Int, dy: Int=>Int) = {
     val x1 = dx(from.x)
     val y1 = dy(from.y)
     val p1 = Position(x1, y1)
+    val p2 = Position(dx(x1), dy(y1))
     List(
-      Move.Sliding(from, p1),
-      Move.Jumping(from, p1, Position(dx(x1), dy(y1))))
+      p1 -> Move.Sliding(from, p1),
+      p2 -> Move.Jumping(from, p1, p2))
   }
 }
--- src/test/scala/io/github/teonistor/draughts/PieceTest.scala
+++ src/test/scala/io/github/teonistor/draughts/PieceTest.scala
@@ -7,31 +7,31 @@ class PieceTest extends AnyFunSuite {
 
   test("kings move where they please") {
     List(Piece.blackKing, Piece.whiteKing).foreach(piece =>
-      assert(piece.emitMoves(Position(3,4)).toSet == Set(
-        Move.Sliding(Position(3,4), Position(4,5)),
-        Move.Sliding(Position(3,4), Position(4,3)),
-        Move.Sliding(Position(3,4), Position(2,5)),
-        Move.Sliding(Position(3,4), Position(2,3)),
-        Move.Jumping(Position(3,4), Position(4,5), Position(5,6)),
-        Move.Jumping(Position(3,4), Position(4,3), Position(5,2)),
-        Move.Jumping(Position(3,4), Position(2,5), Position(1,6)),
-        Move.Jumping(Position(3,4), Position(2,3), Position(1,2)))))
+      assert(piece.emitMoves(Position(3,4)) == Map(
+        Position(4,5) -> Move.Sliding(Position(3,4), Position(4,5)),
+        Position(4,3) -> Move.Sliding(Position(3,4), Position(4,3)),
+        Position(2,5) -> Move.Sliding(Position(3,4), Position(2,5)),
+        Position(2,3) -> Move.Sliding(Position(3,4), Position(2,3)),
+        Position(5,6) -> Move.Jumping(Position(3,4), Position(4,5), Position(5,6)),
+        Position(5,2) -> Move.Jumping(Position(3,4), Position(4,3), Position(5,2)),
+        Position(1,6) -> Move.Jumping(Position(3,4), Position(2,5), Position(1,6)),
+        Position(1,2) -> Move.Jumping(Position(3,4), Position(2,3), Position(1,2)))))
   }
 
   test("black peon moves down") {
-    assert(Piece.blackPeon.emitMoves(Position(1,7)).toSet == Set(
-      Move.Sliding(Position(1,7), Position(0,6)),
-      Move.Sliding(Position(1,7), Position(2,6)),
-      Move.Jumping(Position(1,7), Position(0,6), Position(-1,5)),
-      Move.Jumping(Position(1,7), Position(2,6), Position(3,5))))
+    assert(Piece.blackPeon.emitMoves(Position(1,7)) == Map(
+      Position(0,6) -> Move.Sliding(Position(1,7), Position(0,6)),
+      Position(2,6) -> Move.Sliding(Position(1,7), Position(2,6)),
+      Position(-1,5)-> Move.Jumping(Position(1,7), Position(0,6), Position(-1,5)),
+      Position(3,5) -> Move.Jumping(Position(1,7), Position(2,6), Position(3,5))))
   }
 
   test("white peon moves up") {
-    assert(Piece.whitePeon.emitMoves(Position(1,7)).toSet == Set(
-      Move.Sliding(Position(1,7), Position(0,8)),
-      Move.Sliding(Position(1,7), Position(2,8)),
-      Move.Jumping(Position(1,7), Position(0,8), Position(-1,9)),
-      Move.Jumping(Position(1,7), Position(2,8), Position(3,9))))
+    assert(Piece.whitePeon.emitMoves(Position(1,7)) == Map(
+      Position(0,8) -> Move.Sliding(Position(1,7), Position(0,8)),
+      Position(2,8) -> Move.Sliding(Position(1,7), Position(2,8)),
+      Position(-1,9)-> Move.Jumping(Position(1,7), Position(0,8), Position(-1,9)),
+      Position(3,9) -> Move.Jumping(Position(1,7), Position(2,8), Position(3,9))))
   }
 
   test("kings promote to themselves") {
