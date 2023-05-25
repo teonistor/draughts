46572cdf45930e7fadcf4b55286dacbafb8e94e3
Teodor G Nistor <teodor@nistor.uk>
2023-05-25 22:32:01 +0100

    Make a move on the move of one move

--- src/main/scala/io/github/teonistor/draughts/Game.scala
+++ src/main/scala/io/github/teonistor/draughts/Game.scala
@@ -3,6 +3,7 @@ package io.github.teonistor.draughts
 import io.github.teonistor.draughts.data.{ComputedState, GameState, Position, Settings}
 import io.github.teonistor.draughts.rule.AvailableMovesRule
 import io.vavr.control.Validation
+import io.vavr.control.Validation.invalid
 
 class Game(val availableMovesRule: AvailableMovesRule,
            val settings: Settings, val gameState: GameState) {
@@ -18,8 +19,13 @@ class Game(val availableMovesRule: AvailableMovesRule,
     * dig the map with suitable validations. If all maps, fine, return new Game with state containing other player and board which was dug
     * */
 
-
-    null
+    computedState.availableMoves
+      .get(from)
+      .map(_.getOrElse(to, invalid(s"Your piece from $from cannot reach $to")))
+      .getOrElse(invalid(s"You don't have a piece at $from"))
+    // Here be logic to keep the same player if jump
+      .map(newBoard => GameState(newBoard, gameState.currentPlayer.next))
+      .map(newState => new Game(availableMovesRule, settings, newState))
   }
 
 }
--- src/test/scala/io/github/teonistor/draughts/GameTest.scala
+++ src/test/scala/io/github/teonistor/draughts/GameTest.scala
@@ -3,7 +3,8 @@ package io.github.teonistor.draughts
 import io.github.teonistor.draughts.data.{ComputedState, GameState, Position, Settings}
 import io.github.teonistor.draughts.rule.AvailableMovesRule
 import io.vavr.control.Validation
-import org.junit.jupiter.api.Test
+import io.vavr.control.Validation.{invalid, valid}
+import org.junit.jupiter.api.{BeforeEach, Nested, Test}
 import org.mockito.BDDMockito.`given`
 import org.mockito.Mock
 import org.mockito.Mockito.verifyNoMoreInteractions
@@ -22,4 +23,51 @@ class GameTest extends Assertions {
 
     verifyNoMoreInteractions(availableMovesRule, settings, gameState)
   }
+
+  @Nested
+  class Move {
+    private val nil  = Position(1,2)
+    private val from = Position(2,3)
+    private val good = Position(3,4)
+    private val bad  = Position(4,5)
+
+    private var game: Game =_
+    private var boardAfterMove: Map[Position,Piece] =_
+
+    @BeforeEach
+    def before(@Mock availableMovesRule: AvailableMovesRule, @Mock settings: Settings, @Mock boardAfterMove: Map[Position,Piece]): Unit = {
+      val gameState = GameState(null, Player.black)
+      this.game = new Game(availableMovesRule, settings, gameState)
+      this.boardAfterMove = boardAfterMove
+
+      given(availableMovesRule computeAvailableMoves gameState) willReturn Map(
+        from -> Map(
+          good -> valid(boardAfterMove),
+          bad -> invalid("invalidity from computation")))
+    }
+
+    @Test
+    def cannotMoveFromWhereYouDontHaveAPiece(): Unit = {
+      assert(game.move(nil, good).getError == "You don't have a piece at Position(1,2)")
+    }
+
+    @Test
+    def cannotMoveToWhereYouCantReach(): Unit = {
+      assert(game.move(from, nil).getError == "Your piece from Position(2,3) cannot reach Position(1,2)")
+    }
+
+    @Test
+    def cannotMoveWhereTheMoveIsInvalid(): Unit = {
+      assert(game.move(from, bad).getError == "invalidity from computation")
+    }
+
+    @Test
+    def moveSuccessfully(): Unit = {
+      val newState = game.move(from, good).get.gameState
+      assert(newState.board == boardAfterMove)
+      assert(newState.currentPlayer == Player.white)
+    }
+  }
+
+
 }
