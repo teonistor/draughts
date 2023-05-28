2364ad376b0fcde463c014831939097e2e36dc67
Teodor G Nistor <teodor@nistor.uk>
2023-05-28 19:29:11 +0100

    Refactor

--- src/test/scala/io/github/teonistor/draughts/move/MoveTest.scala
+++ src/test/scala/io/github/teonistor/draughts/move/MoveTest.scala
@@ -5,44 +5,51 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class MoveTest extends AnyFunSuite {
 
+  private val moveMe = "move me"
+  private val ignoreMe = "leave me alone"
+  private val captureMe = "capture me"
+  private val iBlockU = "i block u"
+
+
+
   test("sliding move happens") {
     assert(Move.Sliding(Position(2,2), Position(3,3))
-      .execute(Map(Position(2,2) -> "move me", Position(7,7) -> "leave me alone"))
+      .execute(Map(Position(2,2) -> moveMe, Position(7,7) -> ignoreMe))
       .get ==
-      Map(Position(3,3) -> "move me", Position(7,7) -> "leave me alone"))
+      Map(Position(3,3) -> moveMe, Position(7,7) -> ignoreMe))
   }
 
   test("sliding move refuses because destination is occupied") {
     assert(Move.Sliding(Position(6,6), Position(5,5))
-      .execute(Map(Position(6,6) -> "try to move me", Position(5,5) -> "i block u"))
+      .execute(Map(Position(6,6) -> moveMe, Position(5,5) -> iBlockU))
       .getError ==
       "Position(5,5) is occupied")
   }
 
   test("jumping move happens") {
     assert(Move.Jumping(Position(4,4), Position(5,5), Position(6,6))
-      .execute(Map(Position(4,4) -> "move me", Position(5,5) -> "capture me", Position(7,7) -> "leave me alone"))
+      .execute(Map(Position(4,4) -> moveMe, Position(5,5) -> captureMe, Position(7,7) -> ignoreMe))
       .get ==
-      Map(Position(6,6) -> "move me", Position(7,7) -> "leave me alone"))
+      Map(Position(6,6) -> moveMe, Position(7,7) -> ignoreMe))
   }
 
   test("jumping move refuses because destination is occupied") {
     assert(Move.Jumping(Position(3,4), Position(4,5), Position(5,6))
-      .execute(Map(Position(3,4) -> "try to move me", Position(5,6) -> "i block u"))
+      .execute(Map(Position(3,4) -> moveMe, Position(5,6) -> iBlockU))
       .getError ==
       "Position(5,6) is occupied")
   }
 
   test("jumping move refuses because there's nothing in the 'over' position") {
     assert(Move.Jumping(Position(3,3), Position(4,5), Position(5,7))
-      .execute(Map(Position(3,3) -> "try to move me", Position(4,4) -> "sitting duck"))
+      .execute(Map(Position(3,3) -> moveMe, Position(4,4) -> ignoreMe))
       .getError ==
       "Position(4,5) is not occupied by your opponent")
   }
 
-  test("jumping move refuses what is in the 'over' position isn't capturable") {
+  test("jumping move refuses because what is in the 'over' position isn't capturable") {
     assert(Move.Jumping(Position(3,3), Position(4,4), Position(5,5))
-      .execute(Map(Position(3,3) -> "try to move me", Position(4,4) -> "friendly fire"))
+      .execute(Map(Position(3,3) -> moveMe, Position(4,4) -> moveMe))
       .getError ==
       "Position(4,4) is not occupied by your opponent")
   }
