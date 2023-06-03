55828f204fd3d9a266712a92c8da5a78a9c24842
Teodor G Nistor <teodor@nistor.uk>
2023-06-03 21:50:57 +0100

    Refactor

--- src/main/scala/io/github/teonistor/draughts/InitialBoardProvider.scala
+++ src/main/scala/io/github/teonistor/draughts/InitialBoardProvider.scala
@@ -5,15 +5,14 @@ import io.github.teonistor.draughts.data.{Position, Settings}
 class InitialBoardProvider {
   def createBoard(settings: Settings): Map[Position,Piece] = {
 
-    val mkRow = (y: Int) => (0 until settings.boardWidth)
-      .filter(x => (x + y) % 2 == 0).map(x => Position(x, y))
+    val mkRow = (piece: Piece) => (y: Int) => (0 until settings.boardWidth)
+      .filter(x => (x + y) % 2 == 0)
+      .map(x => (Position(x, y), piece))
 
-    val whitePos = (0 until settings.startingRows)
-      .flatMap(mkRow)
-    val blackPos = (1 to settings.startingRows).map(settings.boardHeight -_)
-      .flatMap(mkRow)
-
-    (blackPos.map((_, Piece.blackPeon)) ++ whitePos.map((_, Piece.whitePeon)))
+    ( (1 to settings.startingRows).map(settings.boardHeight -_)
+        .flatMap(mkRow(Piece.blackPeon)) ++
+      (0 until settings.startingRows)
+        .flatMap(mkRow(Piece.whitePeon)))
       .toMap
   }
 }
