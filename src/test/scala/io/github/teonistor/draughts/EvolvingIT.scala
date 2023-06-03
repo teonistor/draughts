35d14c0b91f9056b273263ca40c3ae0d1702047b
Teodor G Nistor <teodor@nistor.uk>
2023-06-03 21:45:00 +0100

    Slight improvement (evolution?) in EvolvingIT

--- src/test/scala/io/github/teonistor/draughts/EvolvingIT.scala
+++ src/test/scala/io/github/teonistor/draughts/EvolvingIT.scala
@@ -9,33 +9,38 @@ import java.io.{BufferedReader, InputStreamReader}
 object EvolvingIT extends AnyFunSuite{
 
   def main(arg: Array[String]): Unit = {
-    val inputLine = "(-?\\d+) +(-?\\d+) +(-?\\d+) +(-?\\d+)".r
+    val inputLine = " *(-?\\d+) +(-?\\d+) +(-?\\d+) +(-?\\d+) *".r
     val terminalView = new TerminalView()
     var game = new Game(new AvailableMovesRule(),
       new PromotionRule(),
       new GameOverChecker(),
-      Settings(8,8,2),
+      Settings(4,5,2),
       GameState(Map(
         Position(0,0) -> Piece.whitePeon,
         Position(2,0) -> Piece.whitePeon,
         Position(1,1) -> Piece.whitePeon,
-        Position(7,7) -> Piece.blackPeon,
-        Position(5,7) -> Piece.blackPeon,
-        Position(6,6) -> Piece.blackPeon),
+        Position(3,3) -> Piece.blackPeon,
+        Position(1,3) -> Piece.blackPeon,
+        Position(2,2) -> Piece.blackPeon),
         Player.white))
 
     val rd = new BufferedReader(new InputStreamReader(System.in))
-    while(true) {
-      println(terminalView.display(game))
+    println(terminalView.display(game))
 
+    while(true) {
       val mtch = inputLine.pattern.matcher(rd.readLine())
       if (mtch.matches()) {
         val result = game.move(
           Position(mtch.group(1).toInt, mtch.group(2).toInt),
           Position(mtch.group(3).toInt, mtch.group(4).toInt))
-        if (result.isValid) game = result.get
+        if (result.isValid) {
+          game = result.get
+          println(terminalView.display(game))
+        }
         else println(result.getError)
-      }
+
+      } else
+        println("Busted")
     }
   }
 }
