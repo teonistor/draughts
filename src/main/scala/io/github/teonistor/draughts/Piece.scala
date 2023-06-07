cf65ee44d24ed54f0473ad4cf81c284e7b00f39a
Teodor G Nistor <teodor@nistor.uk>
2023-06-07 22:14:48 +0100

    Piece is able to emit jumps only - and look at this composition!

--- src/main/scala/io/github/teonistor/draughts/Piece.scala
+++ src/main/scala/io/github/teonistor/draughts/Piece.scala
@@ -4,42 +4,55 @@ import io.github.teonistor.draughts.data.Position
 import io.github.teonistor.draughts.move.Move
 
 sealed trait Piece {
-  def emitMoves(from: Position): Map[Position, Move]
+  def emitMoves(from:Position): Map[Position, Move]
+  def emitJumps(from:Position): Map[Position, Move]
   def promote: Piece
 }
 
 object Piece {
-  private val `+-1` = LazyList(1, -1)
+  private val `+-1` = List(1, -1)
 
-  // TODO Ideally the concept of "forward" would be relegated to the board (via the specific player) but the static initialisaiton implications of that are asinine
+  // DONTDO Ideally the concept of "forward" would be relegated to the board (via the specific player) but the static initialisaiton implications of that are asinine
 
   val blackKing:Piece = new King
-  val blackPeon:Piece = new Peon(_-1, blackKing)
+  val blackPeon:Piece = new Peon(-1, blackKing)
   val whiteKing:Piece = new King
-  val whitePeon:Piece = new Peon(_+1, whiteKing)
+  val whitePeon:Piece = new Peon(+1, whiteKing)
 
-  private class King extends Piece {
-    override def emitMoves(from: Position): Map[Position, Move] =
+  private trait PieceBase {
+    def emitMoves(from: Position): Map[Position, Move] =
+      emitMovesWith(emitMovesInOneDirection(from, _, _, emitAllMovesInOneDirection))
+
+    def emitJumps(from: Position): Map[Position, Move] =
+      emitMovesWith(emitMovesInOneDirection(from, _, _, emitJumpsInOneDirection))
+
+    protected def emitMovesWith(emission: (Int, Int) => IterableOnce[(Position,Move)]): Map[Position, Move]
+  }
+
+  private class King extends Piece with PieceBase {
+    protected def emitMovesWith(emission: (Int, Int) => IterableOnce[(Position,Move)]): Map[Position, Move] =
       `+-1`.flatMap(dx =>
         `+-1`.flatMap(dy =>
-          emitMovesInOneDirection(from, dx+_, dy+_))).toMap
+          emission(dx, dy))).toMap
 
     override def promote: Piece = this
   }
 
-  private class Peon(forward: Int=>Int, val promote: Piece) extends Piece {
-    override def emitMoves(from: Position): Map[Position, Move] =
-      `+-1`.flatMap(dx =>
-        emitMovesInOneDirection(from, dx+_, forward)).toMap
+  private class Peon(forward: Int, val promote: Piece) extends Piece with PieceBase {
+    protected def emitMovesWith(emission: (Int, Int) => IterableOnce[(Position, Move)]): Map[Position, Move] =
+      `+-1`.flatMap(dx => emission(dx, forward)).toMap
   }
 
-  private def emitMovesInOneDirection(from: Position, dx: Int=>Int, dy: Int=>Int) = {
-    val x1 = dx(from.x)
-    val y1 = dy(from.y)
-    val p1 = Position(x1, y1)
-    val p2 = Position(dx(x1), dy(y1))
-    List(
-      p1 -> Move.Sliding(from, p1),
-      p2 -> Move.Jumping(from, p1, p2))
+  private def emitMovesInOneDirection(from: Position, dx: Int, dy: Int, emission: (Position,Position,Position)=>IterableOnce[(Position,Move)]) = {
+    val x1 = dx + from.x
+    val y1 = dy + from.y
+    emission(from, Position(x1, y1), Position(dx + x1, dy + y1))
   }
+
+  private def emitAllMovesInOneDirection(from: Position, p1: Position, p2: Position) =
+    List(p1 -> Move.Sliding(from, p1),
+         p2 -> Move.Jumping(from, p1, p2))
+
+  private def emitJumpsInOneDirection(from: Position, p1: Position, p2: Position) =
+    Some(p2 -> Move.Jumping(from, p1, p2))
 }
--- src/test/scala/io/github/teonistor/draughts/PieceTest.scala
+++ src/test/scala/io/github/teonistor/draughts/PieceTest.scala
@@ -6,7 +6,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class PieceTest extends AnyFunSuite {
 
-  test("kings move where they please") {
+  test("kings move where they please - all moves") {
     List(Piece.blackKing, Piece.whiteKing).foreach(piece =>
       assert(piece.emitMoves(Position(3,4)) == Map(
         Position(4,5) -> Move.Sliding(Position(3,4), Position(4,5)),
@@ -19,7 +19,7 @@ class PieceTest extends AnyFunSuite {
         Position(1,2) -> Move.Jumping(Position(3,4), Position(2,3), Position(1,2)))))
   }
 
-  test("black peon moves down") {
+  test("black peon moves down - all moves") {
     assert(Piece.blackPeon.emitMoves(Position(1,7)) == Map(
       Position(0,6) -> Move.Sliding(Position(1,7), Position(0,6)),
       Position(2,6) -> Move.Sliding(Position(1,7), Position(2,6)),
@@ -27,10 +27,31 @@ class PieceTest extends AnyFunSuite {
       Position(3,5) -> Move.Jumping(Position(1,7), Position(2,6), Position(3,5))))
   }
 
-  test("white peon moves up") {
+  test("white peon moves up - all moves") {
     assert(Piece.whitePeon.emitMoves(Position(1,7)) == Map(
       Position(0,8) -> Move.Sliding(Position(1,7), Position(0,8)),
       Position(2,8) -> Move.Sliding(Position(1,7), Position(2,8)),
+      Position(-1,9)-> Move.Jumping(Position(1,7), Position(0,8), Position(-1,9)),
+      Position(3,9) -> Move.Jumping(Position(1,7), Position(2,8), Position(3,9))))
+  }
+
+  test("kings move where they please - jumps only") {
+    List(Piece.blackKing, Piece.whiteKing).foreach(piece =>
+      assert(piece.emitJumps(Position(3,4)) == Map(
+        Position(5,6) -> Move.Jumping(Position(3,4), Position(4,5), Position(5,6)),
+        Position(5,2) -> Move.Jumping(Position(3,4), Position(4,3), Position(5,2)),
+        Position(1,6) -> Move.Jumping(Position(3,4), Position(2,5), Position(1,6)),
+        Position(1,2) -> Move.Jumping(Position(3,4), Position(2,3), Position(1,2)))))
+  }
+
+  test("black peon moves down - jumps only") {
+    assert(Piece.blackPeon.emitJumps(Position(1,7)) == Map(
+      Position(-1,5)-> Move.Jumping(Position(1,7), Position(0,6), Position(-1,5)),
+      Position(3,5) -> Move.Jumping(Position(1,7), Position(2,6), Position(3,5))))
+  }
+
+  test("white peon moves up - jumps only") {
+    assert(Piece.whitePeon.emitJumps(Position(1,7)) == Map(
       Position(-1,9)-> Move.Jumping(Position(1,7), Position(0,8), Position(-1,9)),
       Position(3,9) -> Move.Jumping(Position(1,7), Position(2,8), Position(3,9))))
   }
