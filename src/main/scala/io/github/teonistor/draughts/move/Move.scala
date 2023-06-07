0b657caa77b7c09c7d290a28a1bc816446c52dec
Teodor G Nistor <teodor@nistor.uk>
2023-06-07 21:27:46 +0100

    Refactor

--- src/main/scala/io/github/teonistor/draughts/move/Move.scala
+++ src/main/scala/io/github/teonistor/draughts/move/Move.scala
@@ -22,22 +22,12 @@ object Move {
   case class Jumping(from: Position, over: Position, to: Position) extends Move {
     override def execute(gameState: GameState): Validation[String, GameState] =
       validateDestination(gameState, to,
+
         if (capturable(gameState.board))
           valid(GameState(gameState.board.updated(to, gameState.board(from)).removedAll(List(from, over)), gameState.currentPlayer, Some(to)))
 
         else
           invalid(s"$over is not occupied by your opponent"))
-
-//        if (board contains to)
-//        invalid(s"$to is occupied")
-//
-//      // TODO Duplication
-//      // TODO Bonkers way of doing it
-//      else if (board.get(over).exists(p => Player.white.isMyPiece(p) != Player.white.isMyPiece(board(from))))
-//        valid(board.updated(to, board(from)).removedAll(List(from, over)))
-
-//      else
-//        invalid(s"$over is not occupied by your opponent")
 
     private def capturable(board: Map[Position, Piece]) =
       board.get(over).exists(Player.white.isMyPiece(_) != Player.white.isMyPiece(board(from)))
