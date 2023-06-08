be27d9eb51a8fa8ddb1579f11612901c26eb4e94
Teodor G Nistor <teodor@nistor.uk>
2023-06-08 23:06:22 +0100

    EvolvingIT evolves

--- src/test/scala/io/github/teonistor/draughts/EvolvingIT.scala
+++ src/test/scala/io/github/teonistor/draughts/EvolvingIT.scala
@@ -2,6 +2,7 @@ package io.github.teonistor.draughts
 
 import io.github.teonistor.draughts.data.{GameState, Position, Settings}
 import io.github.teonistor.draughts.rule.{AvailableMovesRule, GameOverChecker, PromotionRule}
+import io.vavr.control.Validation
 import org.scalatest.funsuite.AnyFunSuite
 
 import java.io.{BufferedReader, InputStreamReader}
@@ -9,7 +10,7 @@ import java.io.{BufferedReader, InputStreamReader}
 object EvolvingIT extends AnyFunSuite{
 
   def main(arg: Array[String]): Unit = {
-    val inputLine = " *(-?\\d+) +(-?\\d+) +(-?\\d+) +(-?\\d+) *".r
+    val inputLine = "(-?\\d+) +(-?\\d+) +(-?\\d+) +(-?\\d+)".r
     val terminalView = new TerminalView()
     val initialBoardProvider = new InitialBoardProvider()
 
@@ -26,19 +27,27 @@ object EvolvingIT extends AnyFunSuite{
     val rd = new BufferedReader(new InputStreamReader(System.in))
     println(terminalView.display(game))
 
-    while(true) {
-      val mtch = inputLine.pattern.matcher(rd.readLine())
-      if (mtch.matches()) {
-        val result = game.move(
-          Position(mtch.group(1).toInt, mtch.group(2).toInt),
-          Position(mtch.group(3).toInt, mtch.group(4).toInt))
-        if (result.isValid) {
-          game = result.get
-          println(terminalView.display(game))
-        }
-        else println(result.getError)
+    def assignIfValidAndDisplay(result: Validation[String, Game]): Unit = {
+      if (result.isValid) {
+        game = result.get
+        println(terminalView.display(game))
+      }
+      else println(result.getError)
+    }
 
-      } else
+    while (true) {
+      val line = rd.readLine().strip()
+      val mtch = inputLine.pattern.matcher(line)
+
+      if (mtch.matches())
+        assignIfValidAndDisplay(game.move(
+          Position(mtch.group(1).toInt, mtch.group(2).toInt),
+          Position(mtch.group(3).toInt, mtch.group(4).toInt)))
+
+      else if ("pass" equalsIgnoreCase line)
+        assignIfValidAndDisplay(game.pass())
+
+      else
         println("Busted")
     }
   }
