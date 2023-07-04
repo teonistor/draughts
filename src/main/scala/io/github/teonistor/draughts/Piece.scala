94d3e94f9808264ec70d5526043179b39ecacd66
Teodor G Nistor <teodor@nistor.uk>
2023-07-04 21:19:47 +0100

    nd Piece

--- src/main/scala/io/github/teonistor/draughts/Piece.scala
+++ src/main/scala/io/github/teonistor/draughts/Piece.scala
@@ -1,11 +1,12 @@
 package io.github.teonistor.draughts
 
-import io.github.teonistor.draughts.data.Position
 import io.github.teonistor.draughts.move.Move
 
+import scala.annotation.tailrec
+
 sealed trait Piece {
-  def emitMoves(from:Position): Map[Position, Move]
-  def emitJumps(from:Position): Map[Position, Move]
+  def emitMoves(from:Vector[Int]): Map[Vector[Int], Move]
+  def emitJumps(from:Vector[Int]): Map[Vector[Int], Move]
   def promote: Piece
 }
 
@@ -20,39 +21,56 @@ object Piece {
   val whitePeon:Piece = new Peon(+1, whiteKing)
 
   private trait PieceBase {
-    def emitMoves(from: Position): Map[Position, Move] =
-      emitMovesWith(emitMovesInOneDirection(from, _, _, emitAllMovesInOneDirection))
+    def emitMoves(from: Vector[Int]): Map[Vector[Int], Move] = {
+      val firstSteps = fanOut(from)
+      (emitSlidesInOneDirection(from, firstSteps) ++ emitJumpsInOneDirection(from, firstSteps)).toMap
+    }
 
-    def emitJumps(from: Position): Map[Position, Move] =
-      emitMovesWith(emitMovesInOneDirection(from, _, _, emitJumpsInOneDirection))
+    def emitJumps(from: Vector[Int]): Map[Vector[Int], Move] = 
+      emitJumpsInOneDirection(from, fanOut(from)).toMap
 
-    protected def emitMovesWith(emission: (Int, Int) => IterableOnce[(Position,Move)]): Map[Position, Move]
+    protected def fanOut(input: Vector[Int]): Iterable[Vector[Int]]
   }
 
   private class King extends Piece with PieceBase {
-    protected def emitMovesWith(emission: (Int, Int) => IterableOnce[(Position,Move)]): Map[Position, Move] =
-      `+-1`.flatMap(dx =>
-        `+-1`.flatMap(dy =>
-          emission(dx, dy))).toMap
+    protected def fanOut(input: Vector[Int]): Iterable[Vector[Int]] =
+      fanOutRecu(Vector(input), `+-1`)
 
     override def promote: Piece = this
   }
 
   private class Peon(forward: Int, val promote: Piece) extends Piece with PieceBase {
-    protected def emitMovesWith(emission: (Int, Int) => IterableOnce[(Position, Move)]): Map[Position, Move] =
-      `+-1`.flatMap(dx => emission(dx, forward)).toMap
+    protected def fanOut(input: Vector[Int]): Iterable[Vector[Int]] =
+      fanOutRecu(Vector(input), Some(forward))
   }
 
-  private def emitMovesInOneDirection(from: Position, dx: Int, dy: Int, emission: (Position,Position,Position)=>IterableOnce[(Position,Move)]) = {
-    val x1 = dx + from.x
-    val y1 = dy + from.y
-    emission(from, Position(x1, y1), Position(dx + x1, dy + y1))
+  @tailrec
+  private def fanOutRecu(accum: Vector[Vector[Int]], lastOne: Iterable[Int], i: Int = 0): Iterable[Vector[Int]] =
+    if (i < accum.head.size - 1)
+      fanOutRecu(accum.flatMap(v => `+-1`.map(d => v.updated(i, v(i) + d))), lastOne, i + 1)
+    else
+      accum.flatMap(v => lastOne.map(d => v.updated(i, v(i) + d)))
+
+  private def emitSlidesInOneDirection(from: Vector[Int], firstSteps: Iterable[Vector[Int]]) = {
+    firstSteps.map(to => (to, Move.Sliding(from, to)))
+//    to.ma
+//    val x1 = dx + from.x
+//    val y1 = dy + from.y
+//    emission(from, Position(x1, y1), Position(dx + x1, dy + y1))
   }
 
-  private def emitAllMovesInOneDirection(from: Position, p1: Position, p2: Position) =
-    List(p1 -> Move.Sliding(from, p1),
-         p2 -> Move.Jumping(from, p1, p2))
+//  private def emitAllMovesInOneDirection(from: Position, p1: Position, p2: Position) =
+//    List(p1 -> Move.Sliding(from, p1),
+//         p2 -> Move.Jumping(from, p1, p2))
 
-  private def emitJumpsInOneDirection(from: Position, p1: Position, p2: Position) =
-    Some(p2 -> Move.Jumping(from, p1, p2))
+  private def emitJumpsInOneDirection(from: Vector[Int], firstSteps: Iterable[Vector[Int]]) = {
+    firstSteps.map(over => {
+      val to = secondStep(from, over)
+      (to, Move.Jumping(from, over, to))
+    })
+//    Some(p2 -> Move.Jumping(from, p1, p2))
+  }
+
+  private def secondStep(origin:Vector[Int], firstStep:Vector[Int]) =
+    Vector.tabulate(origin.size)(i => 2 * firstStep(i) - origin(i))
 }
--- src/test/scala/io/github/teonistor/draughts/PieceTest.scala
+++ src/test/scala/io/github/teonistor/draughts/PieceTest.scala
@@ -1,59 +1,72 @@
 package io.github.teonistor.draughts
 
-import io.github.teonistor.draughts.data.Position
 import io.github.teonistor.draughts.move.Move
 import org.scalatest.funsuite.AnyFunSuite
 
 class PieceTest extends AnyFunSuite {
 
-  test("kings move where they please - all moves") {
+  test("kings move where they please - all moves - 3D") {
     List(Piece.blackKing, Piece.whiteKing).foreach(piece =>
-      assert(piece.emitMoves(Position(3,4)) == Map(
-        Position(4,5) -> Move.Sliding(Position(3,4), Position(4,5)),
-        Position(4,3) -> Move.Sliding(Position(3,4), Position(4,3)),
-        Position(2,5) -> Move.Sliding(Position(3,4), Position(2,5)),
-        Position(2,3) -> Move.Sliding(Position(3,4), Position(2,3)),
-        Position(5,6) -> Move.Jumping(Position(3,4), Position(4,5), Position(5,6)),
-        Position(5,2) -> Move.Jumping(Position(3,4), Position(4,3), Position(5,2)),
-        Position(1,6) -> Move.Jumping(Position(3,4), Position(2,5), Position(1,6)),
-        Position(1,2) -> Move.Jumping(Position(3,4), Position(2,3), Position(1,2)))))
+      assert(piece.emitMoves(Vector(6,3,4)) == Map(
+        Vector(5,4,5) -> Move.Sliding(Vector(6,3,4), Vector(5,4,5)),
+        Vector(5,4,3) -> Move.Sliding(Vector(6,3,4), Vector(5,4,3)),
+        Vector(5,2,5) -> Move.Sliding(Vector(6,3,4), Vector(5,2,5)),
+        Vector(5,2,3) -> Move.Sliding(Vector(6,3,4), Vector(5,2,3)),
+        Vector(7,4,5) -> Move.Sliding(Vector(6,3,4), Vector(7,4,5)),
+        Vector(7,4,3) -> Move.Sliding(Vector(6,3,4), Vector(7,4,3)),
+        Vector(7,2,5) -> Move.Sliding(Vector(6,3,4), Vector(7,2,5)),
+        Vector(7,2,3) -> Move.Sliding(Vector(6,3,4), Vector(7,2,3)),
+        Vector(4,5,6) -> Move.Jumping(Vector(6,3,4), Vector(5,4,5), Vector(4,5,6)),
+        Vector(4,5,2) -> Move.Jumping(Vector(6,3,4), Vector(5,4,3), Vector(4,5,2)),
+        Vector(4,1,6) -> Move.Jumping(Vector(6,3,4), Vector(5,2,5), Vector(4,1,6)),
+        Vector(4,1,2) -> Move.Jumping(Vector(6,3,4), Vector(5,2,3), Vector(4,1,2)),
+        Vector(8,5,6) -> Move.Jumping(Vector(6,3,4), Vector(7,4,5), Vector(8,5,6)),
+        Vector(8,5,2) -> Move.Jumping(Vector(6,3,4), Vector(7,4,3), Vector(8,5,2)),
+        Vector(8,1,6) -> Move.Jumping(Vector(6,3,4), Vector(7,2,5), Vector(8,1,6)),
+        Vector(8,1,2) -> Move.Jumping(Vector(6,3,4), Vector(7,2,3), Vector(8,1,2)))))
   }
 
   test("black peon moves down - all moves") {
-    assert(Piece.blackPeon.emitMoves(Position(1,7)) == Map(
-      Position(0,6) -> Move.Sliding(Position(1,7), Position(0,6)),
-      Position(2,6) -> Move.Sliding(Position(1,7), Position(2,6)),
-      Position(-1,5)-> Move.Jumping(Position(1,7), Position(0,6), Position(-1,5)),
-      Position(3,5) -> Move.Jumping(Position(1,7), Position(2,6), Position(3,5))))
+    assert(Piece.blackPeon.emitMoves(Vector(1,7)) == Map(
+      Vector(0,6) -> Move.Sliding(Vector(1,7), Vector(0,6)),
+      Vector(2,6) -> Move.Sliding(Vector(1,7), Vector(2,6)),
+      Vector(-1,5)-> Move.Jumping(Vector(1,7), Vector(0,6), Vector(-1,5)),
+      Vector(3,5) -> Move.Jumping(Vector(1,7), Vector(2,6), Vector(3,5))))
   }
 
   test("white peon moves up - all moves") {
-    assert(Piece.whitePeon.emitMoves(Position(1,7)) == Map(
-      Position(0,8) -> Move.Sliding(Position(1,7), Position(0,8)),
-      Position(2,8) -> Move.Sliding(Position(1,7), Position(2,8)),
-      Position(-1,9)-> Move.Jumping(Position(1,7), Position(0,8), Position(-1,9)),
-      Position(3,9) -> Move.Jumping(Position(1,7), Position(2,8), Position(3,9))))
+    assert(Piece.whitePeon.emitMoves(Vector(1,7)) == Map(
+      Vector(0,8) -> Move.Sliding(Vector(1,7), Vector(0,8)),
+      Vector(2,8) -> Move.Sliding(Vector(1,7), Vector(2,8)),
+      Vector(-1,9)-> Move.Jumping(Vector(1,7), Vector(0,8), Vector(-1,9)),
+      Vector(3,9) -> Move.Jumping(Vector(1,7), Vector(2,8), Vector(3,9))))
   }
 
   test("kings move where they please - jumps only") {
     List(Piece.blackKing, Piece.whiteKing).foreach(piece =>
-      assert(piece.emitJumps(Position(3,4)) == Map(
-        Position(5,6) -> Move.Jumping(Position(3,4), Position(4,5), Position(5,6)),
-        Position(5,2) -> Move.Jumping(Position(3,4), Position(4,3), Position(5,2)),
-        Position(1,6) -> Move.Jumping(Position(3,4), Position(2,5), Position(1,6)),
-        Position(1,2) -> Move.Jumping(Position(3,4), Position(2,3), Position(1,2)))))
+      assert(piece.emitJumps(Vector(3,4)) == Map(
+        Vector(5,6) -> Move.Jumping(Vector(3,4), Vector(4,5), Vector(5,6)),
+        Vector(5,2) -> Move.Jumping(Vector(3,4), Vector(4,3), Vector(5,2)),
+        Vector(1,6) -> Move.Jumping(Vector(3,4), Vector(2,5), Vector(1,6)),
+        Vector(1,2) -> Move.Jumping(Vector(3,4), Vector(2,3), Vector(1,2)))))
   }
 
   test("black peon moves down - jumps only") {
-    assert(Piece.blackPeon.emitJumps(Position(1,7)) == Map(
-      Position(-1,5)-> Move.Jumping(Position(1,7), Position(0,6), Position(-1,5)),
-      Position(3,5) -> Move.Jumping(Position(1,7), Position(2,6), Position(3,5))))
+    assert(Piece.blackPeon.emitJumps(Vector(1,7)) == Map(
+      Vector(-1,5)-> Move.Jumping(Vector(1,7), Vector(0,6), Vector(-1,5)),
+      Vector(3,5) -> Move.Jumping(Vector(1,7), Vector(2,6), Vector(3,5))))
   }
 
-  test("white peon moves up - jumps only") {
-    assert(Piece.whitePeon.emitJumps(Position(1,7)) == Map(
-      Position(-1,9)-> Move.Jumping(Position(1,7), Position(0,8), Position(-1,9)),
-      Position(3,9) -> Move.Jumping(Position(1,7), Position(2,8), Position(3,9))))
+  test("white peon moves up - jumps only - 4D") {
+    assert(Piece.whitePeon.emitJumps(Vector(1,0,1,7)) == Map(
+      Vector( 3, 2,-1,9)-> Move.Jumping(Vector(1,0,1,7), Vector(2, 1,0,8), Vector( 3, 2,-1,9)),
+      Vector( 3, 2,3,9) -> Move.Jumping(Vector(1,0,1,7), Vector(2, 1,2,8), Vector( 3, 2,3,9)),
+      Vector( 3,-2,-1,9)-> Move.Jumping(Vector(1,0,1,7), Vector(2,-1,0,8), Vector( 3,-2,-1,9)),
+      Vector( 3,-2,3,9) -> Move.Jumping(Vector(1,0,1,7), Vector(2,-1,2,8), Vector( 3,-2,3,9)),
+      Vector(-1, 2,-1,9)-> Move.Jumping(Vector(1,0,1,7), Vector(0, 1,0,8), Vector(-1, 2,-1,9)),
+      Vector(-1, 2,3,9) -> Move.Jumping(Vector(1,0,1,7), Vector(0, 1,2,8), Vector(-1, 2,3,9)),
+      Vector(-1,-2,-1,9)-> Move.Jumping(Vector(1,0,1,7), Vector(0,-1,0,8), Vector(-1,-2,-1,9)),
+      Vector(-1,-2,3,9) -> Move.Jumping(Vector(1,0,1,7), Vector(0,-1,2,8), Vector(-1,-2,3,9))))
   }
 
   test("kings promote to themselves") {
