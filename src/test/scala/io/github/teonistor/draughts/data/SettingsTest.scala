0258ff57270b4573503337d0341289860a988364
Teodor G Nistor <teodor@nistor.uk>
2023-07-04 19:04:28 +0100

    nd Settings

--- src/test/scala/io/github/teonistor/draughts/data/SettingsTest.scala
+++ src/test/scala/io/github/teonistor/draughts/data/SettingsTest.scala
@@ -4,7 +4,28 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class SettingsTest extends AnyFunSuite {
 
+  test("you must have at least 2 dimensions") {
+    assertThrows[IllegalArgumentException](Settings(1, 4))
+    assertThrows[IllegalArgumentException](Settings(0))
+  }
+
+  test("all dimensions must be greater than 1") {
+    assertThrows[IllegalArgumentException](Settings(1, 1, 2, -1, 3))
+  }
+
+  test("you must have at least 1 starting row") {
+    assertThrows[IllegalArgumentException](Settings(0, 2, 2, 2))
+  }
+
   test("startingRows must be less than half of boardHeight") {
+    assertThrows[IllegalArgumentException](Settings(2, 6, 5, 4))
+    assertThrows[IllegalArgumentException](Settings(4, 9, 8, 7))
+
+    Settings(1, 6, 5, 4)
+    Settings(3, 9, 8, 7)
+  }
+
+  test("startingRows must be less than half of boardHeight - legacy") {
     assertThrows[IllegalArgumentException](Settings(4, 4, 2))
     assertThrows[IllegalArgumentException](Settings(7, 7, 4))
 
