fc6fb15dd169f2b0e37b2946561d3c5067e5dfc0
Teodor G Nistor <teodor@nistor.uk>
2023-07-05 22:06:30 +0100

    TerminalInput reads settings

--- src/main/scala/io/github/teonistor/draughts/TerminalInput.scala
+++ src/main/scala/io/github/teonistor/draughts/TerminalInput.scala
@@ -1,13 +1,13 @@
 package io.github.teonistor.draughts
 
-import io.github.teonistor.draughts.data.Position
+import io.github.teonistor.draughts.data.Settings
 
 import java.io.{BufferedReader, InputStream, InputStreamReader}
 import scala.annotation.tailrec
 import scala.util.Using
 
 class TerminalInput(inputStream: InputStream, juncture: Juncture) extends Runnable {
-  private val moveInput = "(-?\\d+) +(-?\\d+) +(-?\\d+) +(-?\\d+)".r
+  private val moveInput = "[\\d ]+".r
 
   def run(): Unit =
     Using(new BufferedReader(new InputStreamReader(inputStream)))(runLine)
@@ -19,15 +19,33 @@ class TerminalInput(inputStream: InputStream, juncture: Juncture) extends Runnab
     if ("pass" equalsIgnoreCase line)
       juncture.progress(_.pass())
 
-    else {
-      val mtch = moveInput.pattern matcher line
-      if (mtch.matches())
-        juncture.progress(_.move(
-          Position(mtch.group(1).toInt, mtch.group(2).toInt),
-          Position(mtch.group(3).toInt, mtch.group(4).toInt)))
+    else if("new game" equalsIgnoreCase line)
+      juncture.start(readSettings(reader))
 
-      else if (!"exit".equalsIgnoreCase(line))
-        runLine(reader)
+    else if(moveInput matches line) {
+      val u = line.split(" +").map(_.toInt).to(Vector)
+      val (from, to) = u.splitAt(u.length / 2)
+      if (from.size == to.size)
+        juncture.progress(_.move(from, to))
+
     }
+
+    if (!"exit".equalsIgnoreCase(line))
+      runLine(reader)
+  }
+
+  private def readSettings(reader: BufferedReader) = {
+    print("Starting rows (default 2): ")
+    val startingRow = reader.readLine().strip().toIntOption.getOrElse(2)
+    println(startingRow)
+
+    print("Starting rows (default 2): ")
+    val boardSizes = Option(reader.readLine().strip())
+      .filter(moveInput.matches)
+      .map(_.split(" +").map(_.toInt).to(Vector))
+      .getOrElse(Vector(8, 8))
+    println(boardSizes)
+
+    Settings(startingRow, boardSizes:_*)
   }
 }
--- src/test/scala/io/github/teonistor/draughts/TerminalInputTest.scala
+++ src/test/scala/io/github/teonistor/draughts/TerminalInputTest.scala
@@ -1,6 +1,6 @@
 package io.github.teonistor.draughts
 
-import io.github.teonistor.draughts.data.Position
+import io.github.teonistor.draughts.data.Settings
 import io.vavr.control.Validation
 import org.mockito.BDDMockito.{willDoNothing, willReturn}
 import org.mockito.captor.ArgCaptor
@@ -17,6 +17,22 @@ class TerminalInputTest extends AnyFunSuiteLike with IdiomaticMockito {
     new TerminalInput(new ByteArrayInputStream(lines()), mock[Juncture]).run()
   }
 
+  test("new game") {
+    val juncture = mock[Juncture]
+
+    willDoNothing().given(juncture).start(Settings(3, 5, 6, 7))
+
+    new TerminalInput(new ByteArrayInputStream(lines("new game", "3", "5 6  7")), juncture).run()
+  }
+
+  test("new game with defaults") {
+    val juncture = mock[Juncture]
+
+    willDoNothing().given(juncture).start(Settings(2, Seq(8, 8):_*))
+
+    new TerminalInput(new ByteArrayInputStream(lines("new game", " ", "")), juncture).run()
+  }
+
   test("pass") {
     val juncture = mock[Juncture]
     val gameIn = mock[Game]
@@ -31,25 +47,28 @@ class TerminalInputTest extends AnyFunSuiteLike with IdiomaticMockito {
     assert(gameFunc.value(gameIn) == gameOut)
   }
 
-  test("move") {
-    val juncture = mock[Juncture]
-    val gameIn = mock[Game]
-    val gameOut = mock[Validation[String, Game]]
-    val gameFunc = ArgCaptor[Game=>Validation[String,Game]]
+  LazyList(("1 2 3 4",        Vector(1, 2),          Vector(3, 4)),
+      ("1 2 3 4 5 6",         Vector(1, 2, 3),       Vector(4, 5, 6)),
+      ("1 2 3 4 5 6 7 8 9 0", Vector(1, 2, 3, 4, 5), Vector(6, 7, 8, 9, 0)))
+    .foreach { case (fromTo, from, to) => test("move " + fromTo) {
+      val juncture = mock[Juncture]
+      val gameIn = mock[Game]
+      val gameOut = mock[Validation[String, Game]]
+      val gameFunc = ArgCaptor[Game => Validation[String, Game]]
 
-    willDoNothing().given(juncture).progress(gameFunc)
-    willReturn(gameOut).given(gameIn).move(Position(1,2), Position(3,4))
+      willDoNothing().given(juncture).progress(gameFunc)
+      willReturn(gameOut).given(gameIn).move(from, to)
 
-    new TerminalInput(new ByteArrayInputStream(lines("1 2 3 4")), juncture).run()
+      new TerminalInput(new ByteArrayInputStream(lines(fromTo)), juncture).run()
 
-    assert(gameFunc.value(gameIn) == gameOut)
-  }
+      assert(gameFunc.value(gameIn) == gameOut)
+    }}
 
   private def lines(lns: String*) =
     lns
-      .prepended("garbage")
+      .prepended("garbage")  // Implicitly test that garbage input is ignored
       .appended("exit")
-      .map(" " * between(1,4) +_+ " " * between(1,4))
+      .map(" " * between(1,4) +_+ " " * between(1,4))  // Implicitly test that leading and trailing white space is trimmed
       .mkString("\n")
       .getBytes(UTF_8)
 }
