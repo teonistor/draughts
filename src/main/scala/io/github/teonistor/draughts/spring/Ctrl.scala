114f91a23f5d3572a12c710e89e45e7c16b1ca2a
Teodor G Nistor <teodor@nistor.uk>
2023-07-16 22:26:24 +0100

    Setting up WS comms, hard to tell

---
+++ src/main/scala/io/github/teonistor/draughts/spring/Ctrl.scala
@@ -0,0 +1,66 @@
+package io.github.teonistor.draughts.spring
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.teonistor.draughts.data.Settings
+import io.github.teonistor.draughts.{Game, HDUtils, Juncture, Piece, Player, View}
+import org.springframework.context.annotation.Lazy
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.messaging.simp.annotation.SubscribeMapping
+import org.springframework.web.bind.annotation.{RequestMapping, RestController}
+
+@Lazy
+@RestController
+@RequestMapping(Array("draughts-api"))
+class Ctrl(om: ObjectMapper, ws: SimpMessagingTemplate, junctureFactory: View=>Juncture) extends View {
+
+  private lazy val juncture = junctureFactory(this)
+
+  // Intermediate UI cache, so that a client joining midway sees the state right away
+  private var lastState: SendableState =_
+  private var lastSettings: SendableSettings =_
+
+  override def announce(message: String): Unit =
+    ws.convertAndSend("/draughts/draughts-message", message)
+
+  override def display(game: Game): Unit = {
+    lastState = SendableState(
+      game.gameState.board.to(LazyList).map { case (position,piece) => val (a,b) = position.splitAt(position.size - 3); (a, b, piece) },
+      game.gameState.currentPlayer,
+      game.gameState.ongoingJump,
+      game.isGameOver)
+    ws.convertAndSend("/draughts/draughts-state", lastState)
+  }
+
+  @MessageMapping(Array("/click"))
+  def receive(message: (Vector[Int],Vector[Int])): Unit =
+      juncture.progress(game => game.move _ tupled message)
+
+  @MessageMapping(Array("/pass"))
+  def receive(message: String): Unit =
+    juncture.progress(_.pass())
+
+  @MessageMapping(Array("/new-game"))
+  def receive(settings: Settings): Unit = {
+    juncture.start(settings)
+    lastSettings = SendableSettings(
+      settings.boardSizes,
+      HDUtils.cartesianProduct(settings.boardSizes.take(settings.boardSizes.size - 3).to(Vector).map(0 until settings.boardSizes(_))))
+    ws.convertAndSend("/draughts/draughts-settings", lastSettings)
+  }
+
+  @SubscribeMapping(Array("/draughts-state"))
+  def onSubscribeState = lastState
+
+  @SubscribeMapping(Array("/draughts-settings"))
+  def onSubscribeSettings = lastSettings
+
+
+  case class SendableSettings(boardSizes:Seq[Int],
+                              partialIndices:Seq[Vector[Int]])
+
+  case class SendableState(board: LazyList[(Vector[Int], Vector[Int], Piece)],
+                           currentPlayer: Player,
+                           ongoingJump: Option[Vector[Int]],
+                           isGameOver: Boolean)
+}
--- src/main/vue/src/game.vue
+++ src/main/vue/src/game.vue
@@ -3,9 +3,15 @@
   <v-app>
     <v-container>
       <v-row>
-        <v-col md="4" v-for="partialIndex in partialIndices">
+        <v-col md="4">
 
-          <p>Board at {{ JSON.stringify(partialIndex) }}</p>
+          <v-text-field /> <v-text-field />
+          <v-btn @click="newGame">New game</v-btn>
+
+        </v-col>
+
+        <v-col md="4" v-for="partialIndex in partialIndices">
+          <p>Board at ({{ partialIndex.join(', ') }}, z, x, y)</p>
 
           <board3D :depth="3"
                    :width="4"
@@ -20,6 +26,9 @@
   </v-app>
 </template>
 <script>
+  import SockJS from 'sockjs-client';
+  import Stomp from 'stompjs';
+
   import board3D from './components/board3D.vue';
 
   const testBoard = '[[[1,2,3,4],"BP"],[[0,1,0,3],"BP"],[[0,0,2,4],"BP"],[[1,0,0,3],"BK"],[[1,2,0,3],"BP"],[[0,0,3,3],"BP"],[[1,1,3,3],"BP"],[[1,0,3,4],"BP"],[[1,2,3,0],"WP"],[[1,1,3,1],"WK"],[[0,0,1,1],"WP"],[[0,1,2,3],"BP"],[[1,1,2,4],"BP"],[[1,1,0,4],"BP"],[[0,2,2,4],"BP"],[[0,0,1,3],"BP"],[[1,0,1,4],"BP"],[[1,1,0,0],"WP"],[[0,2,2,0],"WP"],[[1,0,1,0],"WP"],[[0,1,1,4],"BP"],[[0,0,0,4],"BP"],[[0,1,3,0],"WP"],[[0,0,0,0],"WP"],[[0,2,0,0],"WP"],[[0,2,0,4],"BP"],[[1,2,2,3],"BP"],[[1,2,1,4],"BP"],[[1,0,2,3],"BP"],[[1,2,0,1],"WP"],[[0,2,1,1],"WP"],[[0,0,3,1],"WP"],[[0,1,1,0],"WP"],[[1,0,2,1],"WP"],[[1,0,0,1],"WP"],[[0,0,2,0],"WP"],[[0,1,3,4],"BP"],[[1,1,1,1],"WP"],[[1,0,3,0],"WP"],[[0,2,3,1],"WP"],[[0,2,3,3],"BP"],[[1,2,1,0],"WP"],[[1,2,2,1],"WP"],[[1,1,1,3],"BP"],[[1,1,2,0],"WP"],[[0,2,1,3],"BP"],[[0,1,2,1],"WP"],[[0,1,0,1],"WP"]]';
@@ -30,8 +39,18 @@
     components: {board3D},
 
     data: () => ({
+      // From settings
+      boardSizes: [],
+      partialIndices: [[0], [1]],
+
+      // From state
       board: JSON.parse(testBoard),
-      partialIndices: [[0], [1]]
+      currentPlayer: null,
+      ongoingJump: null,
+      isGameOver: false,
+
+      // Other...
+      stompClient: null
     }),
 
     computed: {
@@ -39,6 +58,25 @@
     },
 
     methods: {
+
+      connect () {
+        let socket = new SockJS(this.$backendRoot + '/draughts-subscribe');
+        this.stompClient = Stomp.over(socket);
+        this.stompClient.connect({}, frame => {
+          this.stompClient.subscribe('/draughts/draughts-state', this.receiveState);
+          this.stompClient.subscribe('/draughts/draughts-settings', this.receiveSettings);
+          this.stompClient.subscribe('/draughts/draughts-message', this.receiveMessage);
+        });
+
+        // Poor man's callback chain
+        let stompOnClose = socket.onclose;
+        socket.onclose = status => {
+          stompOnClose(status);
+          this.stompClient = null;
+        }
+      },
+
+
       section (partialIndex) {
         const data = {};
         this.board.forEach(kv => {
@@ -53,11 +91,38 @@
           return -1;
         else
           return this.board[0][0].slice(partialIndex.length).reduce((a,b) => a + b) % 2;
+      },
+
+
+      newGame () {
+        this.stompClient.send("/draughts/new-game", {}, JSON.stringify({
+          startingRows: 2,
+          boardSizes: [5, 5, 5]
+        }));
+      },
+
+
+      receiveState (message) {
+        const state = JSON.parse(message.body);
+        this.board = state.board;
+        this.currentPlayer = state.currentPlayer;
+        this.ongoingJump = state.ongoingJump;
+        this.isGameOver = state.isGameOver;
+      },
+
+      receiveSettings (message) {
+        const settings = JSON.parse(message.body);
+        this.boardSizes = settings.boardSizes;
+        this.partialIndices = settings.partialIndices;
+      },
+
+      receiveMessage (message) {
+
       }
     },
 
     mounted () {
-
+      this.connect();
     }
   }
 </script>
