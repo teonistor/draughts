4003ac5b38fbca5d5425e3a6c7fa4b4a51b50c07
Teodor G Nistor <teodor@nistor.uk>
2023-08-20 18:51:09 +0100

    Look at this beautiful New Game dialog!

--- src/main/vue/src/components/newGameControls.vue
+++ src/main/vue/src/components/newGameControls.vue
@@ -10,25 +10,44 @@
 
       <v-expansion-panel-content class="outer-panel">
 
-        <v-btn @click="newGame">New game</v-btn>
-        with the following settings:
-        <v-text-field v-model="startingRowsInput" label="Starting rows"/>
-        <v-text-field v-model="boardSizesInput" label="Sizes"/>
+        Starting rows:<br>
+        <v-text-field type="number" style="width:100px" v-model="startingRows" />
 
-<!-- the privileged dimension with respect to which pieces are initially placed, and have to move forward until promoted -->
+        Board sizes: ({{ boardSizes.length }} dimensions)<br>
+        <v-btn @click="addDimension">+</v-btn>
+        <v-btn @click="removeDimension">-</v-btn>
+
+        <p v-for="boardSize in boardSizes">
+          <v-text-field type="number" style="width:100px; display: inline-block" v-model="boardSize.value" />
+          &emsp;–&ensp;{{ boardSize.description }}
+        </p>
+
+        <v-btn @click="newGame">New game</v-btn>
 
       </v-expansion-panel-content>
     </v-expansion-panel>
   </v-expansion-panels>
 </template>
 <script>
+  const descriptions = [
+    'board height – the privileged dimension with respect to which pieces are initially placed, and have to move forward until promoted',
+    'board width',
+    'board depth',
+    'meta height',
+    'meta width'
+  ];
+  const defaultDescription = 'some higher dimension';
+
   export default {
     name: 'newGameControls',
-    props: ['stompClient', 'startingRows', 'boardSizes'],
+    props: ['stompClient', 'startingRowsIn', 'boardSizesIn'],
 
     data: () => ({
       expanded: null,
-      startingRowsInput: '',
+
+      startingRows: 1,
+      boardSizes: [],
+
       boardSizesInput: ''
     }),
 
@@ -36,10 +55,21 @@
     },
 
     methods: {
+      addDimension () {
+        this.boardSizes = [{
+          value: 3,
+          description: descriptions[this.boardSizes.length] || defaultDescription
+        }].concat(this.boardSizes);
+      },
+
+      removeDimension () {
+        this.boardSizes = this.boardSizes.slice(1);
+      },
+
       newGame () {
         this.stompClient.send("/draughts/new-game", {}, JSON.stringify({
-          startingRows: parseInt(this.startingRowsInput),
-          boardSizes: this.boardSizesInput.split(" ").map(e => parseInt(e))
+          startingRows: parseInt(this.startingRows),
+          boardSizes: this.boardSizes.map(e => parseInt(e.value))
         }));
         this.expanded = null;
       }
@@ -49,6 +79,9 @@
     },
 
     mounted () {
+      this.addDimension();
+      this.addDimension();
+      this.addDimension();
     }
   }
 </script>
