a34bab1ae46ff445a119894e148bd7df2532a7d8
Teodor G Nistor <teodor@nistor.uk>
2024-05-12 20:56:44 +0100

    Connect 4 (n) GameOverChecker - refactor

--- src/main/scala/io/github/teonistor/connectn/data/GameOverChecker.scala
+++ src/main/scala/io/github/teonistor/connectn/data/GameOverChecker.scala
@@ -2,15 +2,16 @@ package io.github.teonistor.connectn.data
 
 import io.github.teonistor.connectn.Game
 import io.github.teonistor.connectn.data.GameState.Board
-import io.github.teonistor.draughts.HDUtils
 import io.github.teonistor.draughts.HDUtils.cartesianProduct
+
+import scala.collection.Iterator.iterate
 
 object GameOverChecker {
 
-  def isGameOver(game: Game): (Boolean, Option[Color]) = {
-
+  def isGameOver(game: Game): (Boolean, Option[Color]) =
     game.history.map(_.board) match {
-      case current::previous::_ =>
+
+      case current :: previous :: _=>
         val lastMove = current.keysIterator
           .find(k => !previous.contains(k) || current(k).size > previous(k).size)
 
@@ -20,28 +21,30 @@ object GameOverChecker {
 
         val lastColumn = lastMove.map(current).get
         val lastPosition = lastMove.get :+ lastColumn.size - 1
+
         if (current.forall(_._2.size >= game.settings.height))
-          return (true, None)
+          (true, None)
 
-        val existsWin = cartesianProduct(Vector.fill(game.settings.baseDimensions.size + 1)(Vector(-1, 0, 1))).iterator
-          .filterNot(_.forall(_==0))
-          .exists { delta =>
-            Iterator.iterate(lastPosition.toList)(_.zip(delta).map { case (a, b) => a + b })
-              .take(game.settings.winningThreshold)
-              .map { case idx :: base =>
-                current.getOrElse(base.to(Vector), List.empty).lift(idx)
-              }
-              .toSet
-              .size == 1
-          }
-
-        if (existsWin)
+        else if (cartesianProduct(Vector.fill(game.settings.baseDimensions.size + 1)(Vector(-1, 0, 1))).iterator
+            .filterNot(_.forall(_ == 0))
+            .exists(hasStreak(current, lastPosition, _, game.settings.winningThreshold)))
           (true, Some(lastColumn.head))
+
         else
           (false, None)
 
-      // No history. We assume that means an empty board, because I said so.
-      case _   => (false,None)
+      // No history. We assume that means nobody won yet, because I said so.
+      case _=> (false, None)
     }
-  }
+
+  private def hasStreak(board: Board, position: Vector[Int], direction: Vector[Int], winningThreshold: Int) =
+    iterate(position.toList)(_.zip(direction).map(addPair))
+      .take(winningThreshold)
+      .map { case idx :: base =>
+        board.getOrElse(base.to(Vector), List.empty).lift(idx) }
+      .toSet
+      .size == 1
+
+  private def addPair(pair:(Int,Int)) =
+    pair._1 + pair._2
 }
