4d7828a82cc4e9daac108af838c70c3e7ce1c4ea
Teodor G Nistor <teodor@nistor.uk>
2024-11-20 23:17:44 +0000

    Ship in play also has a name

--- bsms/src/main/scala/io/github/teonistor/bsms/data/ShipInPlay.scala
+++ bsms/src/main/scala/io/github/teonistor/bsms/data/ShipInPlay.scala
@@ -2,7 +2,8 @@ package io.github.teonistor.bsms.data
 
 import io.github.teonistor.bsms.data.OceanCell.damagedShip
 
-case class ShipInPlay(parts: Map[Position, ShipCell]) {
+case class ShipInPlay(name: String,
+                      parts: Map[Position, ShipCell]) {
 
   lazy val isCompletelyDamaged: Boolean = parts.values.forall(_== damagedShip)
 }
--- bsms/src/test/scala/io/github/teonistor/bsms/data/ShipInPlayTest.scala
+++ bsms/src/test/scala/io/github/teonistor/bsms/data/ShipInPlayTest.scala
@@ -8,15 +8,19 @@ class ShipInPlayTest extends AnyFunSuiteLike with IdiomaticMockito {
 
   private val (p, q) = (mock[Position], mock[Position])
 
+  test("has name") {
+    assert(ShipInPlay("Ghost ship", Map.empty).name == "Ghost ship")
+  }
+
   test("not damaged") {
-    assert(!ShipInPlay(Map(p -> healthyShip, q -> healthyShip)).isCompletelyDamaged)
+    assert(!ShipInPlay("", Map(p -> healthyShip, q -> healthyShip)).isCompletelyDamaged)
   }
 
   test("a bit damaged but not completely") {
-    assert(!ShipInPlay(Map(p -> healthyShip, q -> damagedShip)).isCompletelyDamaged)
+    assert(!ShipInPlay("", Map(p -> healthyShip, q -> damagedShip)).isCompletelyDamaged)
   }
 
   test("completely damaged") {
-    assert(ShipInPlay(Map(p -> damagedShip, q -> damagedShip)).isCompletelyDamaged)
+    assert(ShipInPlay("", Map(p -> damagedShip, q -> damagedShip)).isCompletelyDamaged)
   }
 }
