8c2984f539ff70e9cdf7abada994e3446a59ec73
Teodor G Nistor <teodor@nistor.uk>
2024-11-21 23:33:43 +0000

    MinePlacementRule - damage a ship

--- bsms/src/main/scala/io/github/teonistor/bsms/rule/MinePlacementRule.scala
+++ bsms/src/main/scala/io/github/teonistor/bsms/rule/MinePlacementRule.scala
@@ -1,13 +1,23 @@
 package io.github.teonistor.bsms.rule
 
-import io.github.teonistor.bsms.data.OceanCell.mine
+import io.github.teonistor.bsms.data.OceanCell.{damagedShip, mine}
 import io.github.teonistor.bsms.data.{OwnBoard, Position}
 import io.vavr.control.Validation
 import io.vavr.control.Validation.valid
 
 object MinePlacementRule {
 
-  def placeMine(ownBoard: OwnBoard, position: Position): Validation[String, OwnBoard] = {
-    valid(ownBoard + (position -> Left(mine)))
+  def placeMine(board: OwnBoard, position: Position): Validation[String, OwnBoard] = {
+    val u = board.get(position)
+      .flatMap(_.toOption)
+//      .map(ship => {
+//        val updatedShip = ship.copy(parts = ship.parts + (position -> damagedShip))
+//        updatedShip
+//      })
+      .map(ship => ship.copy(parts=ship.parts + (position -> damagedShip)))
+      .map(ship => ship.parts.keys.map(_-> Right(ship)))
+      .getOrElse(Some(position -> Left(mine)))
+
+    valid(board ++ u)
   }
 }
--- bsms/src/test/scala/io/github/teonistor/bsms/rule/MinePlacementRuleTest.scala
+++ bsms/src/test/scala/io/github/teonistor/bsms/rule/MinePlacementRuleTest.scala
@@ -1,11 +1,12 @@
 package io.github.teonistor.bsms.rule
 
-import io.github.teonistor.bsms.data.OceanCell.mine
+import io.github.teonistor.bsms.data.OceanCell.{damagedShip, healthyShip, mine}
+import io.github.teonistor.bsms.data.ShipInPlay
 import org.scalatest.funsuite.AnyFunSuiteLike
 
 class MinePlacementRuleTest extends AnyFunSuiteLike {
 
-  test("place a mine") {
+  test("place a mine in water") {
     val position = Vector(3, 7)
 
     val result = MinePlacementRule.placeMine(Map.empty, position)
@@ -16,6 +17,19 @@ class MinePlacementRuleTest extends AnyFunSuiteLike {
 // What to do about keeping info about the opponent's board isn't so straightforward; deal with it later
 //    assert(result.get._1 == Map(position -> Left(mine)))
 //    assert(result.get._2 == Map(position -> miss))
+  }
+
+  test("place mine on ship") {
+    val initialShip = ShipInPlay("Fishing Boat", Map(Vector(7, 2) -> healthyShip, Vector(7, 3) -> healthyShip))
+    val expectedShip = ShipInPlay("Fishing Boat", Map(Vector(7, 2) -> healthyShip, Vector(7, 3) -> damagedShip))
+
+    val result = MinePlacementRule.placeMine(
+      Map(Vector(7, 2) -> Right(initialShip), Vector(7, 3) -> Right(initialShip)),
+      Vector(7, 3))
+
+    assert(result.isValid)
+    assert(result.get()(Vector(7, 2)) == Right(expectedShip))
+    assert(result.get()(Vector(7, 3)) == Right(expectedShip))
   }
 
 }
