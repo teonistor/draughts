da2d9354992b4b4ff5c9bcc9763f071ce538984c
Teodor G Nistor <teodor@nistor.uk>
2024-11-21 23:58:33 +0000

    MinePlacementRule - optimisation & tests

--- bsms/src/main/scala/io/github/teonistor/bsms/rule/MinePlacementRule.scala
+++ bsms/src/main/scala/io/github/teonistor/bsms/rule/MinePlacementRule.scala
@@ -1,23 +1,20 @@
 package io.github.teonistor.bsms.rule
 
-import io.github.teonistor.bsms.data.OceanCell.{damagedShip, mine}
+import io.github.teonistor.bsms.data.OceanCell.{damagedShip, healthyShip, mine}
 import io.github.teonistor.bsms.data.{OwnBoard, Position}
 import io.vavr.control.Validation
 import io.vavr.control.Validation.valid
 
 object MinePlacementRule {
 
-  def placeMine(board: OwnBoard, position: Position): Validation[String, OwnBoard] = {
-    val u = board.get(position)
+  def placeMine(board: OwnBoard, position: Position): Validation[String, OwnBoard] =
+    valid(board ++ board
+      .get(position)
       .flatMap(_.toOption)
-//      .map(ship => {
-//        val updatedShip = ship.copy(parts = ship.parts + (position -> damagedShip))
-//        updatedShip
-//      })
+      .map(ship => Some(ship)
+      .filter(_.parts(position) == healthyShip)
       .map(ship => ship.copy(parts=ship.parts + (position -> damagedShip)))
       .map(ship => ship.parts.keys.map(_-> Right(ship)))
-      .getOrElse(Some(position -> Left(mine)))
-
-    valid(board ++ u)
-  }
+      .getOrElse(None))
+      .getOrElse(Some(position -> Left(mine))))
 }
--- bsms/src/test/scala/io/github/teonistor/bsms/rule/MinePlacementRuleTest.scala
+++ bsms/src/test/scala/io/github/teonistor/bsms/rule/MinePlacementRuleTest.scala
@@ -12,14 +12,24 @@ class MinePlacementRuleTest extends AnyFunSuiteLike {
     val result = MinePlacementRule.placeMine(Map.empty, position)
 
     assert(result.isValid)
-    assert(result.get == Map(position -> Left(mine)))
+    assert(result.contains(Map(position -> Left(mine))))
 
 // What to do about keeping info about the opponent's board isn't so straightforward; deal with it later
 //    assert(result.get._1 == Map(position -> Left(mine)))
 //    assert(result.get._2 == Map(position -> miss))
   }
 
-  test("place mine on ship") {
+  test("place a mine on a mine changes nothing") {
+    val position = Vector(2, 4)
+    val board = Map(position -> Left(mine))
+
+    val result = MinePlacementRule.placeMine(board, position)
+
+    assert(result.isValid)
+    assert(result.contains(board))
+  }
+
+  test("place a mine on healthy part of ship damages it") {
     val initialShip = ShipInPlay("Fishing Boat", Map(Vector(7, 2) -> healthyShip, Vector(7, 3) -> healthyShip))
     val expectedShip = ShipInPlay("Fishing Boat", Map(Vector(7, 2) -> healthyShip, Vector(7, 3) -> damagedShip))
 
@@ -32,4 +42,15 @@ class MinePlacementRuleTest extends AnyFunSuiteLike {
     assert(result.get()(Vector(7, 3)) == Right(expectedShip))
   }
 
+  test("place a mine on damaged part of ship changes nothing") {
+    val ship = ShipInPlay("Fishing Boat", Map(Vector(7, 5) -> damagedShip, Vector(7, 6) -> healthyShip))
+
+    val result = MinePlacementRule.placeMine(
+      Map(Vector(7, 2) -> Right(ship), Vector(7, 3) -> Right(ship)),
+      Vector(7, 5))
+
+    assert(result.isValid)
+    assert(result.get()(Vector(7, 2)) == Right(ship))
+    assert(result.get()(Vector(7, 3)) == Right(ship))
+  }
 }
