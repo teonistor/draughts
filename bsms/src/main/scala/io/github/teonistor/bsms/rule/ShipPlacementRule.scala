44e4cd169efcdaa858eef2850262cca4b47d3e1c
Teodor G Nistor <teodor@nistor.uk>
2024-11-21 18:46:20 +0000

    ShipPlacementRule - place ship horizontally vs vertically

--- bsms/src/main/scala/io/github/teonistor/bsms/rule/ShipPlacementRule.scala
+++ bsms/src/main/scala/io/github/teonistor/bsms/rule/ShipPlacementRule.scala
@@ -9,13 +9,15 @@ import io.vavr.control.Validation.valid
 object ShipPlacementRule {
 
   def placeShip(board: OwnBoard, ship: ShipDescription, position: Position, orientation: Orientation): Validation[String, OwnBoard] = {
-     val tp = (0 until ship.length)
-       .map(d => position.zipWithIndex.map {
-         case (coord, i) => if (i == 1) coord + d else coord
-       })
+    val tp = (0 until ship.length)
+      .map(d => position.zipWithIndex.map(p => (p, orientation) match {
+        case ((coord, 0), Orientation.horizontal) => coord + d
+        case ((coord, 1), Orientation.vertical) => coord + d
+        case ((coord, _), _) => coord
+      }))
 
-      val tb = ShipInPlay(ship.name, tp.map((_, healthyShip)).toMap)
+    val tb = ShipInPlay(ship.name, tp.map((_, healthyShip)).toMap)
 
-      valid(board ++ tp.map((_, Right(tb))))
+    valid(board ++ tp.map((_, Right(tb))))
   }
 }
--- bsms/src/test/scala/io/github/teonistor/bsms/rule/ShipPlacementRuleTest.scala
+++ bsms/src/test/scala/io/github/teonistor/bsms/rule/ShipPlacementRuleTest.scala
@@ -1,6 +1,6 @@
 package io.github.teonistor.bsms.rule
 
-import io.github.teonistor.bsms.core.Orientation.horizontal
+import io.github.teonistor.bsms.core.Orientation.{horizontal, vertical}
 import io.github.teonistor.bsms.data.OceanCell.healthyShip
 import io.github.teonistor.bsms.data.{ShipDescription, ShipInPlay}
 import org.scalatest.funsuite.AnyFunSuiteLike
@@ -9,7 +9,24 @@ class ShipPlacementRuleTest extends AnyFunSuiteLike {
 
   private val fishingBoat = ShipDescription("Fishing Boat", 2)
 
-  test("place ship") {
+  test("place ship horizontally") {
+    val positionsWhereShipWillBe = List(
+      Vector(4, 8), Vector(5, 8))
+    val positionsWhereShipWontBe = List(
+      Vector(3, 7), Vector(3, 8), Vector(3, 9),
+      Vector(4, 7), Vector(4, 9),
+      Vector(5, 7), Vector(5, 9),
+      Vector(6, 7), Vector(6, 8), Vector(6, 9))
+    val expectedShip = ShipInPlay("Fishing Boat", positionsWhereShipWillBe.map((_, healthyShip)).toMap)
+
+    val result = ShipPlacementRule.placeShip(Map.empty, fishingBoat, Vector(4,8), horizontal)
+
+    assert(result.isValid)
+    positionsWhereShipWillBe.foreach(p => assert(result.get()(p) == Right(expectedShip)))
+    positionsWhereShipWontBe.foreach(p => assert(!result.get().contains(p)))
+  }
+
+  test("place ship vertically") {
     val positionsWhereShipWillBe = List(
       Vector(1, 2), Vector(1, 3))
     val positionsWhereShipWontBe = List(
@@ -18,7 +35,7 @@ class ShipPlacementRuleTest extends AnyFunSuiteLike {
       Vector(2, 1), Vector(2, 2), Vector(2, 3), Vector(2, 4))
     val expectedShip = ShipInPlay("Fishing Boat", positionsWhereShipWillBe.map((_, healthyShip)).toMap)
 
-    val result = ShipPlacementRule.placeShip(Map.empty, fishingBoat, Vector(1,2), horizontal)
+    val result = ShipPlacementRule.placeShip(Map.empty, fishingBoat, Vector(1,2), vertical)
 
     assert(result.isValid)
     positionsWhereShipWillBe.foreach(p => assert(result.get()(p) == Right(expectedShip)))
